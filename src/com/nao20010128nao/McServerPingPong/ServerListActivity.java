72cd3e0f593fa529dd505ab5a44fd2f11f821826
nao20010128nao <nao20010128@gmail.com>
2015-12-09 23:12:17 +0900

    削除

--- src/com/nao20010128nao/McServerPingPong/ServerListActivity.java
+++ src/com/nao20010128nao/McServerPingPong/ServerListActivity.java
@@ -42,14 +42,6 @@ public class ServerListActivity extends ListActivity{
 		getListView().setLongClickable(true);
 		pref=PreferenceManager.getDefaultSharedPreferences(this);
 		loadServers();
-		/*Server s=new Server();
-		s.ip="setsuna.info";
-		s.port=19132;
-		sl.add(s);
-		s=new Server();
-		s.ip="sg.lbsg.net";
-		s.port=19132;
-		sl.add(s);*/
 	}
 	@Override
 	protected void onDestroy() {
