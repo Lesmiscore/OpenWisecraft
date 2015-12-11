07efb599cc739284fad1030286cefc7f17129e69
nao20010128nao <nao20010128@gmail.com>
2015-12-12 00:36:39 +0900

    いろいろ

--- src/com/nao20010128nao/McServerPingPong/Consistant.java
+++ src/com/nao20010128nao/McServerPingPong/Consistant.java
@@ -1,7 +1,11 @@
 package com.nao20010128nao.McServerPingPong;
+import java.util.*;
 
 public class Consistant
 {
 	public static final int ACTIVITY_RESULT_UPDATE=5;
 	public static final int ACTIVITY_RESULT_DELETE=6;
+	public static final List ONE_LENGTH_NULL_LIST=Collections.unmodifiableList(Arrays.asList(new Object[1]));
+	public static final List TEN_LENGTH_NULL_LIST=Collections.unmodifiableList(Arrays.asList(new Object[10]));
+	public static final List ONE_HUNDRED_LENGTH_NULL_LIST=Collections.unmodifiableList(Arrays.asList(new Object[100]));
 }
---
+++ src/com/nao20010128nao/McServerPingPong/Utils.java
@@ -0,0 +1,19 @@
+package com.nao20010128nao.McServerPingPong;
+
+public class Utils
+{
+	public static String deleteDecorations(String decorated) {
+		StringBuilder sb=new StringBuilder();
+		char[] chars=decorated.toCharArray();
+		int offset=0;
+		while (chars.length > offset) {
+			if (chars[offset] == '§') {
+				offset += 2;
+				continue;
+			}
+			sb.append(chars[offset]);
+			offset++;
+		}
+		return sb.toString();
+	}
+}
