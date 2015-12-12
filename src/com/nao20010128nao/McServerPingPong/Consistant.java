bb1c85f8104bfa5d6fa754cc9f8595eacf426022
nao20010128nao <nao20010128@gmail.com>
2015-12-12 10:00:30 +0900

    追加

--- src/com/nao20010128nao/McServerPingPong/Consistant.java
+++ src/com/nao20010128nao/McServerPingPong/Consistant.java
@@ -8,4 +8,5 @@ public class Consistant
 	public static final List ONE_LENGTH_NULL_LIST=Collections.unmodifiableList(Arrays.asList(new Object[1]));
 	public static final List TEN_LENGTH_NULL_LIST=Collections.unmodifiableList(Arrays.asList(new Object[10]));
 	public static final List ONE_HUNDRED_LENGTH_NULL_LIST=Collections.unmodifiableList(Arrays.asList(new Object[100]));
+	public static final List ONE_THOUSAND_LENGTH_NULL_LIST=Collections.unmodifiableList(Arrays.asList(new Object[1000]));
 }
