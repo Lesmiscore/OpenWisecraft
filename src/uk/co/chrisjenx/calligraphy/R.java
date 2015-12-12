13c02387373b913d2b5f118e59d78c37de648614
nao20010128nao <nao20010128@gmail.com>
2015-12-12 14:10:18 +0900

    エラー対策

--- src/uk/co/chrisjenx/calligraphy/R.java
+++ src/uk/co/chrisjenx/calligraphy/R.java
@@ -5,4 +5,7 @@ public class R
 	public static class attr{
 		public static final int fontPath=com.nao20010128nao.McServerPingPong.R.attr.fontPath;
 	}
+	public static class id{
+		public static final int calligraphy_tag_id=com.nao20010128nao.McServerPingPong.R.id.calligraphy_tag_id;
+	}
 }
