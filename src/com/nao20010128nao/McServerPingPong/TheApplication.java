42deaf5a135c66914262085e783f394843daf5e0
nao20010128nao <nao20010128@gmail.com>
2015-12-12 13:39:08 +0900

    フォント読み込み

--- src/com/nao20010128nao/McServerPingPong/TheApplication.java
+++ src/com/nao20010128nao/McServerPingPong/TheApplication.java
@@ -1,6 +1,17 @@
 package com.nao20010128nao.McServerPingPong;
 import android.app.*;
+import android.graphics.*;
 
 public class TheApplication extends Application
 {
+	public static TheApplication instance;
+	public static Typeface cinzelDecorative,latoLight;
+	@Override
+	public void onCreate() {
+		// TODO: Implement this method
+		super.onCreate();
+		instance=this;
+		cinzelDecorative=Typeface.createFromAsset(getAssets(),"cinzeldecorative.ttf");
+		latoLight=Typeface.createFromAsset(getAssets(),"lato-light.ttf");
+	}
 }
