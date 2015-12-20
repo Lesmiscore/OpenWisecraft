8196347f1dc4c3954e90a03c4b2e42a0ee8eb61f
nao20010128nao <nao20010128@gmail.com>
2015-12-21 00:34:55 +0900

    修正

--- src/com/nao20010128nao/McServerPingPong/rcon/buttonActions/DefaultGamemode.java
+++ src/com/nao20010128nao/McServerPingPong/rcon/buttonActions/DefaultGamemode.java
@@ -32,4 +32,10 @@ public class DefaultGamemode extends NameSelectAction
 		// TODO: Implement this method
 		return R.id.defaultgamemode;
 	}
+
+	@Override
+	public String[] onPlayersList() {
+		// TODO: Implement this method
+		return getResources().getStringArray(R.array.defaultgamemodeConst);
+	}
 }
