2c99bf83634d122b64b7d932f5677485f901d931
nao20010128nao <nao20010128@gmail.com>
2016-01-01 17:20:38 +0900

    再編成

--- src/com/nao20010128nao/Wisecraft/api/ApiActions.java
+++ src/com/nao20010128nao/Wisecraft/api/ApiActions.java
@@ -2,12 +2,24 @@ package com.nao20010128nao.Wisecraft.api;
 
 public class ApiActions
 {
-	private static String ACTION_BASE="com.nao20010128nao.Wisecraft.";
-	public static String SERVER_INFO=ACTION_BASE+"SERVER_INFO";
-	public static String RCON_ACCESS=ACTION_BASE+"RCON_ACCESS";
+	private final static String ACTION_BASE="com.nao20010128nao.Wisecraft.";
+	public  final static String SERVER_INFO=ACTION_BASE+"SERVER_INFO";
+	
+	private final static String SERVER_INFO_HIDE=ACTION_BASE+"SERVER_INFO_HIDE_";
+	
+	public  final static String SERVER_INFO_IP=ACTION_BASE+"SERVER_IP";
+	public  final static String SERVER_INFO_PORT=ACTION_BASE+"SERVER_PORT";
+	public  final static String SERVER_INFO_ISPC=ACTION_BASE+"SERVER_ISPC";
+	
+	public  final static String SERVER_INFO_HIDE_DETAILS=SERVER_INFO_HIDE+"DETAILS";
+	public  final static String SERVER_INFO_HIDE_PLAYERS=SERVER_INFO_HIDE+"PLAYERS";
+	public  final static String SERVER_INFO_HIDE_PLUGINS=SERVER_INFO_HIDE+"PLUGINS";
+	public  final static String SERVER_INFO_DISABLE_UPDATE=SERVER_INFO_HIDE+"UPDATE";
 	
 	
-	public static String SERVER_INFO_IP=ACTION_BASE+"SERVER_IP";
-	public static String SERVER_INFO_PORT=ACTION_BASE+"SERVER_PORT";
-	public static String SERVER_INFO_ISPC=ACTION_BASE+"SERVER_ISPC";
+	private final static String RCON=ACTION_BASE+"RCON";
+	
+	public  final static String RCON_ACCESS=RCON+"_ACCESS";
+	
+	public  final static String RCON_PASSWORD=RCON+"_PASSWORD";
 }
