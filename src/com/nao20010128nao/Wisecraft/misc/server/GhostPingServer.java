032d0b8fb642b07aac75ad97fb46b171c03b0275
nao20010128nao <nao20010128@gmail.com>
2016-01-09 18:02:30 +0900

    途中経過

--- src/com/nao20010128nao/Wisecraft/misc/server/GhostPingServer.java
+++ src/com/nao20010128nao/Wisecraft/misc/server/GhostPingServer.java
@@ -25,7 +25,9 @@ public class GhostPingServer extends Thread
 			}
 		}
 		while(true){
-			DatagramPacket pak=new DatagramPacket(Factories.byteArray(100*1024),0,1024*100);
+			byte[] ba=Factories.byteArray(100*1024);
+			Arrays.fill(ba,(byte)-1);
+			DatagramPacket pak=new DatagramPacket(ba,0,1024*100);
 			socket.receive(pak);
 			check(pak);
 		}
@@ -52,6 +54,53 @@ public class GhostPingServer extends Thread
 		int sessionId=dis.readInt();
 		if(type==0){
 			//stat
+			Log.d("ghost_query","len="+p.getLength());
+			int pad=dis.readInt();
+			ByteArrayOutputStream result=new ByteArrayOutputStream();
+			DataOutputStream resW=new DataOutputStream(result);
+			if(pad==0){
+				//full stat
+				resW.write(0);resW.writeInt(sessionId);
+				//73 70 6C 69 74 6E 75 6D 00 80 00
+				resW.write((byte)0x73);
+				resW.write((byte)0x70);
+				resW.write((byte)0x6c);
+				resW.write((byte)0x69);
+				resW.write((byte)0x74);
+				resW.write((byte)0x6e);
+				resW.write((byte)0x75);
+				resW.write((byte)0x6d);
+				resW.write((byte)0x00);
+				resW.write((byte)0x80);
+				resW.write((byte)0x00);
+				//KV
+				resW.write("gametype\0SMP\0".getBytes(StandardCharsets.US_ASCII));//gametype
+				resW.write("map\0wisecraft\0".getBytes(StandardCharsets.US_ASCII));//map
+				resW.write("server_engine\0Wisecraft Ghost Ping\0".getBytes(StandardCharsets.US_ASCII));//server_engine
+				resW.write(("hostport\0"+localPort+"\0").getBytes(StandardCharsets.US_ASCII));//hostport
+				resW.write("whitelist\0on\0".getBytes(StandardCharsets.US_ASCII));//whitelist
+				resW.write(("plugins\0Wisecraft Ghost Ping"+buildPlugins()+"\0").getBytes(StandardCharsets.US_ASCII));//server_engine
+				resW.write("hostname\0§5Wisecraft\0".getBytes(StandardCharsets.UTF_8));//whitelist
+				resW.write("numplayers\00\0".getBytes(StandardCharsets.UTF_8));//whitelist
+				resW.write("version\0v0.13.1 alpha\0".getBytes(StandardCharsets.UTF_8));//whitelist
+				resW.write("game_id\0MINECRAFTPE\0".getBytes(StandardCharsets.UTF_8));//whitelist
+				resW.write("hostip\00.0.0.0\0".getBytes(StandardCharsets.UTF_8));//whitelist
+				resW.write("maxplayers\00\0".getBytes(StandardCharsets.UTF_8));//whitelist
+				//01 70 6C 61 79 65 72 5F 00 00
+				resW.write((byte)0x01);
+				resW.write((byte)0x70);
+				resW.write((byte)0x6c);
+				resW.write((byte)0x69);
+				resW.write((byte)0x74);
+				resW.write((byte)0x6e);
+				resW.write((byte)0x75);
+				resW.write((byte)0x6d);
+				resW.write((byte)0x00);
+				resW.write((byte)0x00);
+				
+			}else{
+				//basic stat
+			}
 		}else if(type==9){
 			//handshake
 			ByteArrayOutputStream result=new ByteArrayOutputStream();
@@ -65,5 +114,8 @@ public class GhostPingServer extends Thread
 			resP.setSocketAddress(p.getSocketAddress());
 			socket.send(resP);
 		}
+	}
+	String buildPlugins(){
+		return "";
 	}
 }
