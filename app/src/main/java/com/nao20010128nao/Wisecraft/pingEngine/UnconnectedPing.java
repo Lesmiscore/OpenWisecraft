dff6604d1f8bee47b87a9baa177d87b7f02fa1f6
nao20010128nao <nao20010128@gmail.com>
2016-02-11 15:30:50 +0900

    作成中

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/pingEngine/UnconnectedPing.java
@@ -0,0 +1,57 @@
+package com.nao20010128nao.Wisecraft.pingEngine;
+import com.nao20010128nao.MCPing.*;
+import java.net.*;
+import java.io.*;
+
+public class UnconnectedPing
+{
+	public static final byte UCP_PID=0x01;
+	public static final long MAGIC_1ST=0x00ffff00fefefefe;
+	public static final long MAGIC_2ND=0xfdfdfdfd12345678;
+	
+	public static UnconnectedPingResult doPing(String ip,int port,boolean throwError)throws IOException{
+		DatagramSocket ds=null;
+		try{
+			ds=new DatagramSocket();
+			ByteArrayOutputStream baos=new ByteArrayOutputStream(25);
+			DataOutputStream dos=new DataOutputStream(baos);
+			dos.write(UCP_PID);
+			dos.writeLong(System.currentTimeMillis());
+			dos.writeLong(MAGIC_1ST);
+			dos.writeLong(MAGIC_2ND);
+			dos.flush();
+			DatagramPacket dp=new DatagramPacket(baos.toByteArray(),baos.size(),InetAddress.getByName(ip),port);
+			ds.send(dp);
+			
+			DatagramPacket recDp=new DatagramPacket(new byte[1000],1000);
+			ds.receive(recDp);
+			byte[] recvBuf=new byte[recDp.getLength()];
+			System.arraycopy(recDp.getData(),0,recvBuf,0,recvBuf.length);
+			
+			DataInputStream dis=new DataInputStream(new ByteArrayInputStream(recvBuf));
+			if(dis.readByte()!=0x1c){
+				throw new IOException("Server replied with invalid data");
+			}
+			dis.readLong();//Ping ID
+			dis.readLong();//Server ID
+			dis.readLong();//MAGIC
+			dis.readLong();//MAGIC
+			String s=dis.readUTF();
+		}catch(IOException e){
+			throw e;
+		}finally{
+			if(ds!=null)ds.close();
+		}
+		return null;
+	}
+	
+	public static class UnconnectedPingResult implements ServerPingResult{
+		String[] serverInfos;
+		public UnconnectedPingResult(String s){
+			serverInfos=s.split("\\;");
+		}
+		public String getServerName(){
+			return serverInfos[0];
+		}
+	}
+}
