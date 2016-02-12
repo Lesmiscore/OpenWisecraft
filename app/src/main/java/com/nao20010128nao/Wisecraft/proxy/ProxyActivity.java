cfbed7096c0e5fe9dcfd4ca8607b96a66da852fb
nao20010128nao <nao20010128@gmail.com>
2016-02-12 17:41:51 +0900

    いろいろ

--- app/src/main/java/com/nao20010128nao/Wisecraft/proxy/ProxyActivity.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/proxy/ProxyActivity.java
@@ -1,6 +1,24 @@
 package com.nao20010128nao.Wisecraft.proxy;
 import android.app.*;
+import android.os.*;
+import com.nao20010128nao.Wisecraft.*;
+import com.nao20010128nao.MCProxy.*;
+import android.widget.*;
 
 public class ProxyActivity extends Activity
 {
+	LoggerProxy prox;
+	TextView serverIp,serverCon;
+	String ip;
+	int port;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.proxy_screen);
+		serverIp=(TextView)findViewById(R.id.serverIp);
+		serverCon=(TextView)findViewById(R.id.serverCon);
+		ip=getIntent().getStringExtra("ip");
+		port=getIntent().getIntExtra("port",19132);
+	}
 }
