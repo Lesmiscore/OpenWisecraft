a01d3a235886927506dee84789143f61939a1725
nao20010128nao <nao20010128@gmail.com>
2016-02-18 21:03:41 +0900

    追加

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.nao20010128nao.Wisecraft;
+import android.preference.PreferenceActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends PreferenceActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+	}
+	
+}
