6e093fc1e0ed6e296e76b0bc1c7bc43b4b069571
nao20010128nao <nao20010128@gmail.com>
2016-03-10 16:50:40 +0900

    追加

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/ServerFinder.java
@@ -0,0 +1,13 @@
+package com.nao20010128nao.Wisecraft;
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class ServerFinder extends ListActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+	}
+}
