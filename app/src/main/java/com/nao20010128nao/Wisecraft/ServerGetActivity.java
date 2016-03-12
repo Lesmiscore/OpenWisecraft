195a69a1a5820d435dc60584415832143b208af5
nao20010128nao <nao20010128@gmail.com>
2016-03-12 18:16:52 +0900

    追加

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/ServerGetActivity.java
@@ -0,0 +1,20 @@
+package com.nao20010128nao.Wisecraft;
+import com.nao20010128nao.Wisecraft.misc.WebViewActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class ServerGetActivity extends WebViewActivity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO: Implement this method
+		
+		return true;
+	}
+}
