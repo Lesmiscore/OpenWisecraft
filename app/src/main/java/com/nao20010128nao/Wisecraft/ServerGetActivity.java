95d1de69a02a56a7c94a1b2b3633f77c0c580782
nao20010128nao <nao20010128@gmail.com>
2016-03-13 15:35:57 +0900

    追加

--- app/src/main/java/com/nao20010128nao/Wisecraft/ServerGetActivity.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/ServerGetActivity.java
@@ -2,6 +2,9 @@ package com.nao20010128nao.Wisecraft;
 import com.nao20010128nao.Wisecraft.misc.WebViewActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.util.Log;
 
 public class ServerGetActivity extends WebViewActivity
 {
@@ -9,12 +12,33 @@ public class ServerGetActivity extends WebViewActivity
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
+		new AlertDialog.Builder(this)
+			.setSingleChoiceItems(R.array.serverListSites,-1,new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface di,int w){
+					di.dismiss();
+					loadUrl(getResources().getStringArray(R.array.serverListSites)[w]);
+				}
+			})
+			.setTitle(R.string.selectWebSite)
+			.setOnCancelListener(new DialogInterface.OnCancelListener(){
+				public void onCancel(DialogInterface di){
+					finish();
+					Log.d("SGA","cancel");
+				}
+			})
+			.setOnDismissListener(new DialogInterface.OnDismissListener(){
+				public void onDismiss(DialogInterface di){
+					finish();
+					Log.d("SGA","dismiss");
+				}
+			})
+			.show();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// TODO: Implement this method
-		
+		menu.add(Menu.NONE,0,0,R.string.findServers);
 		return true;
 	}
 }
