31ba9dd4ad48c3b07e632a1713ad3adc9e34fdbc
nao20010128nao <nao20010128@gmail.com>
2016-04-08 08:38:00 +0900

    途中

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/skin_face/SkinFaceFetcher.java
@@ -0,0 +1,29 @@
+package com.nao20010128nao.Wisecraft.misc.skin_face;
+import android.graphics.Bitmap;
+
+public class SkinFaceFetcher
+{
+	SkinFetcher sf=new SkinFetcher();
+	public void requestLoadSkin(String player,SkinFetcher.SkinFetchListener listener){
+		sf.requestLoadSkin(player,new LoaderListener(player,listener));
+	}
+	class LoaderListener implements SkinFetcher.SkinFetchListener {
+		String player;
+		SkinFetcher.SkinFetchListener lis;
+		public LoaderListener(String name,SkinFetcher.SkinFetchListener lis){
+			player=name;
+			this.lis=lis;
+		}
+
+		@Override
+		public void onError(String player) {
+			// TODO: Implement this method
+			lis.onError(player);
+		}
+
+		@Override
+		public void onSuccess(Bitmap bmp, String player) {
+			// TODO: Cut image here
+		}
+	}
+}
