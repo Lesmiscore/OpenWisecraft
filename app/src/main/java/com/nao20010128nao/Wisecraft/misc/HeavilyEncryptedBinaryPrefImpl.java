e943d95e5d0946661f76302c48a102f14e7161ee
nao20010128nao <nao20010128@gmail.com>
2016-04-11 16:48:44 +0900

    実装待ち

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/HeavilyEncryptedBinaryPrefImpl.java
@@ -0,0 +1,35 @@
+package com.nao20010128nao.Wisecraft.misc;
+
+public class HeavilyEncryptedBinaryPrefImpl extends BinaryPrefImpl
+{
+	/*
+	 public EncryptedBinaryPref() {
+	 // TODO 自動生成されたコンストラクター・スタブ
+	 super();
+	 }
+
+	 public EncryptedBinaryPref(Map<String, ?> map) {
+	 // TODO 自動生成されたコンストラクター・スタブ
+	 super(map);
+	 }
+
+	 public EncryptedBinaryPref(File f) throws IOException {
+	 // TODO 自動生成されたコンストラクター・スタブ
+	 this(readAllFromFile(f));
+	 }
+
+	 public EncryptedBinaryPref(byte[] b) throws IOException {
+	 // TODO 自動生成されたコンストラクター・スタブ
+	 this(readAllFromBytes(b));
+	 }
+
+	 public EncryptedBinaryPref(InputStream is) throws IOException {
+	 this(is, true);
+	 }
+
+	 public EncryptedBinaryPref(InputStream is, boolean close) throws IOException {
+	 this(readAllFromStream(is, close));
+	 }
+	 
+	*/
+}
