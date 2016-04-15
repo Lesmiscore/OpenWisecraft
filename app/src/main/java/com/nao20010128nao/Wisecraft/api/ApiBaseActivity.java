e0e7737ca0d276a23f6ad55846c05105470f6501
nao20010128nao <nao20010128@gmail.com>
2016-04-15 20:34:17 +0900

    置き換え

--- app/src/main/java/com/nao20010128nao/Wisecraft/api/ApiBaseActivity.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/api/ApiBaseActivity.java
@@ -1,10 +1,10 @@
 package com.nao20010128nao.Wisecraft.api;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public abstract class ApiBaseActivity extends FragmentActivity {
+public abstract class ApiBaseActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
