c15e77c3daec624c4b4cae4af0b1ffa2c27d96d2
nao20010128nao <nao20010128@gmail.com>
2016-04-18 08:23:25 +0900

    変更

--- app/src/main/java/com/nao20010128nao/Wisecraft/misc/compat/CompatArrayAdapter.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/compat/CompatArrayAdapter.java
@@ -5,13 +5,11 @@ import java.util.Collection;
 public class CompatArrayAdapter
 {
 	public static <T> void addAll(ArrayAdapter<T> adapter,T[] array){
-		for(T o:array){
+		for(T o:array)
 			adapter.add(o);
-		}
 	}
 	public static <T> void addAll(ArrayAdapter<T> adapter,Collection<T> array){
-		for(T o:array){
+		for(T o:array)
 			adapter.add(o);
-		}
 	}
 }
