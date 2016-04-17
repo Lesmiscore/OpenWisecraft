6365367abb48404c74c199445c67643255026ce2
nao20010128nao <nao20010128@gmail.com>
2016-04-18 07:13:03 +0900

    実装

--- app/src/main/java/com/nao20010128nao/Wisecraft/misc/TorChecker.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/TorChecker.java
@@ -4,12 +4,18 @@ import org.jsoup.nodes.Document;
 import java.net.URL;
 import java.io.IOException;
 import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
+import android.util.Log;
 
 public class TorChecker
 {
 	public static boolean checkTor1()throws IOException{
-		Document html=Jsoup.parse(new URL("http://check.torproject.org/?lang=en_US"),1000);
-		Elements torStat=html.select("h1");
+		Document html=Jsoup.connect("http://check.torproject.org/?lang=en_US").userAgent("Mozilla").get();
+		Element torStat=html.select("div>h1").get(0);
+		Log.d("TorChecker",torStat.text());
+		if(torStat.text().toLowerCase().contains("Conguratulations")){
+			return true;
+		}
 		return false;
 	}
 }
