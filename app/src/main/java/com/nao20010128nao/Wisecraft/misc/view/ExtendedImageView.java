25dc7abd4563ac83c6df713beee20c33ce85c8d6
nao20010128nao <nao20010128@gmail.com>
2016-04-18 15:49:58 +0900

    いろいろ修正

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/view/ExtendedImageView.java
@@ -0,0 +1,28 @@
+package com.nao20010128nao.Wisecraft.misc.view;
+import android.support.v7.widget.AppCompatImageView;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.graphics.drawable.ColorDrawable;
+
+public class ExtendedImageView extends AppCompatImageView
+{
+	public ExtendedImageView(Context context) {
+		super(context);
+	}
+
+    public ExtendedImageView(Context context, AttributeSet attrs) {
+		super(context,attrs);
+	}
+
+    public ExtendedImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context,attrs,defStyleAttr);
+	}
+	
+	public void setColor(int color){
+		setImageDrawable(new ColorDrawable(color));
+	}
+	
+	public void setColorRes(int les){
+		setColor(getResources().getColor(les));
+	}
+}
