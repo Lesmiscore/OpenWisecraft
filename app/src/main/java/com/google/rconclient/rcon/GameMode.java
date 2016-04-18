3488e2c9be17104243483ef13c0c5b5b5065980d
nao20010128nao <nao20010128@gmail.com>
2016-04-18 12:40:16 +0900

    いらないけど追加

--- app/src/main/java/com/google/rconclient/rcon/GameMode.java
+++ app/src/main/java/com/google/rconclient/rcon/GameMode.java
@@ -1,7 +1,7 @@
 package com.google.rconclient.rcon;
 
 public enum GameMode {
-	Survival(0), Creative(1);
+	Survival(0), Creative(1), Adventure(2), Spectator(3);
 	private final int number;
 	private GameMode(final int number) {
 		this.number = number;
