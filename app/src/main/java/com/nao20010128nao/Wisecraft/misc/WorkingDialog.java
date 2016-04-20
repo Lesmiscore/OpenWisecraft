f1bb1b7bb3f2530e053b2a15287a9ae731653edd
nao20010128nao <nao20010128@gmail.com>
2016-04-21 00:52:23 +0900

    AppCompat適用

--- app/src/main/java/com/nao20010128nao/Wisecraft/misc/WorkingDialog.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/WorkingDialog.java
@@ -3,6 +3,15 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.ContextWrapper;
 import com.nao20010128nao.Wisecraft.R;
+import android.content.Context;
+import android.support.v7.app.AppCompatDelegate;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v7.app.AppCompatDialog;
 
 public class WorkingDialog extends ContextWrapper {
 	ProgressDialog waitDialog;
@@ -16,7 +25,7 @@ public class WorkingDialog extends ContextWrapper {
 		if (waitDialog != null) {
 			hideWorkingDialog();
 		}
-		waitDialog = new ProgressDialog(this);
+		waitDialog = new AppCompatProgressDialog(this);
 		waitDialog.setIndeterminate(true);
 		waitDialog.setMessage(message);
 		waitDialog.setCancelable(false);
@@ -28,5 +37,60 @@ public class WorkingDialog extends ContextWrapper {
 		}
 		waitDialog.cancel();
 		waitDialog = null;
+	}
+	
+	class AppCompatProgressDialog extends ProgressDialog{
+		AppCompatDelegate dlg;
+		public AppCompatProgressDialog(Context ctx){
+			super(ctx);
+			dlg=AppCompatDelegate.create(this,null);
+			dlg.installViewFactory();
+		}
+
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			dlg.installViewFactory();
+			dlg.onCreate(savedInstanceState);
+			super.onCreate(savedInstanceState);
+		}
+		
+		public ActionBar getSupportActionBar() {
+			return dlg.getSupportActionBar();
+		}
+
+		public void setSupportActionBar(Toolbar toolbar) {
+			dlg.setSupportActionBar(toolbar);
+		}
+
+		@Override
+		public void setContentView(int layoutResID) {
+			dlg.setContentView(layoutResID);
+		}
+
+		@Override
+		public void setContentView(View view) {
+			dlg.setContentView(view);
+		}
+
+		@Override
+		public void setContentView(View view, ViewGroup.LayoutParams params) {
+			dlg.setContentView(view, params);
+		}
+
+		@Override
+		public void addContentView(View view, ViewGroup.LayoutParams params) {
+			dlg.addContentView(view, params);
+		}
+
+		@Override
+		protected void onStop() {
+			super.onStop();
+			dlg.onStop();
+		}
+
+		public void invalidateOptionsMenu() {
+			dlg.invalidateOptionsMenu();
+		}
 	}
 }
