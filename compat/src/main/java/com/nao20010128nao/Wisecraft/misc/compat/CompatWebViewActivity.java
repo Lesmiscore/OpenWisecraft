464a2471cd6eb299badf80980d770159294ea484
nao20010128nao <nao20010128@gmail.com>
2016-04-21 08:50:01 +0900

    変更

--- compat/src/main/java/com/nao20010128nao/Wisecraft/misc/compat/CompatWebViewActivity.java
+++ compat/src/main/java/com/nao20010128nao/Wisecraft/misc/compat/CompatWebViewActivity.java
@@ -1,9 +1,10 @@
 package com.nao20010128nao.Wisecraft.misc.compat;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public abstract class CompatWebViewActivity extends SimpleCompatActivity
+public abstract class CompatWebViewActivity extends AppCompatActivity
 {
 	WebView webView;
 	@Override
