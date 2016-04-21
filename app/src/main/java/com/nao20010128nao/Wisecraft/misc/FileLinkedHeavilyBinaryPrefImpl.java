a10ddfa59f91be846c36fbed161a854c86e25ee8
nao20010128nao <nao20010128@gmail.com>
2016-04-21 16:43:16 +0900

    追加

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/FileLinkedHeavilyBinaryPrefImpl.java
@@ -0,0 +1,35 @@
+package com.nao20010128nao.Wisecraft.misc;
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+import java.util.HashSet;
+import com.nao20010128nao.Wisecraft.Utils;
+
+public class FileLinkedHeavilyBinaryPrefImpl extends HeavilyEncryptedBinaryPrefImpl
+{
+	File fileDir;
+	public FileLinkedHeavilyBinaryPrefImpl(File f)throws IOException{
+		super(f);
+		fileDir=f;
+	}
+	
+	private class BpiEdt extends PreferencesEditorWrapper{
+		public BpiEdt(){
+			super(FileLinkedHeavilyBinaryPrefImpl.super.edit());
+		}
+
+		@Override
+		public boolean commit() {
+			// TODO: Implement this method
+			return super.commit()&Utils.writeToFileByBytes(fileDir,toBytes());
+		}
+
+		@Override
+		public void apply() {
+			// TODO: Implement this method
+			super.apply();
+			Utils.writeToFileByBytes(fileDir,toBytes());
+		}
+		
+	}
+}
---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/PreferencesEditorWrapper.java
@@ -0,0 +1,80 @@
+package com.nao20010128nao.Wisecraft.misc;
+import android.content.SharedPreferences.Editor;
+import java.util.Set;
+import java.util.HashSet;
+
+public class PreferencesEditorWrapper implements Editor {
+	Editor child;
+
+	public PreferencesEditorWrapper(Editor c){
+		child=c;
+	}
+	
+	@Override
+	public Editor putString(String key, String value) {
+		// TODO 自動生成されたメソッド・スタブ
+		child.putString(key,value);
+		return this;
+	}
+
+	@Override
+	public Editor putStringSet(String key, Set<String> values) {
+		// TODO 自動生成されたメソッド・スタブ
+		child.putStringSet(key,new HashSet<>(values));
+		return this;
+	}
+
+	@Override
+	public Editor putInt(String key, int value) {
+		// TODO 自動生成されたメソッド・スタブ
+		child.putInt(key,value);
+		return this;
+	}
+
+	@Override
+	public Editor putLong(String key, long value) {
+		// TODO 自動生成されたメソッド・スタブ
+		child.putLong(key,value);
+		return this;
+	}
+
+	@Override
+	public Editor putFloat(String key, float value) {
+		// TODO 自動生成されたメソッド・スタブ
+		child.putFloat(key,value);
+		return this;
+	}
+
+	@Override
+	public Editor putBoolean(String key, boolean value) {
+		// TODO 自動生成されたメソッド・スタブ
+		child.putBoolean(key,value);
+		return this;
+	}
+
+	@Override
+	public Editor remove(String key) {
+		// TODO 自動生成されたメソッド・スタブ
+		child.remove(key);
+		return this;
+	}
+
+	@Override
+	public Editor clear() {
+		// TODO 自動生成されたメソッド・スタブ
+		child.clear();
+		return this;
+	}
+
+	@Override
+	public boolean commit() {
+		// TODO 自動生成されたメソッド・スタブ
+		return child.commit();
+	}
+
+	@Override
+	public void apply() {
+		// TODO 自動生成されたメソッド・スタブ
+		child.apply();
+	}
+}
