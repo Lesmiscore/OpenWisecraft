0efb5894c8f19d7bdfbd17e2a156a818825785fd
nao20010128nao <nao20010128@gmail.com>
2016-04-25 12:52:53 +0900

    追加

--- app/src/main/java/com/nao20010128nao/Wisecraft/misc/RhinoExec.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/RhinoExec.java
@@ -10,6 +10,7 @@ public class RhinoExec extends Thread
 		// TODO: Implement this method
 		Context ctx=Context.enter();
 		try{
+			ctx.setOptimizationLevel(-1);
 			Scriptable scr=ctx.initStandardObjects();
 			ctx.evaluateString(scr,"android.util.Log.i(\"RhinoExec\",\"It looks like there's no problem in the Javascript executor.\");","<data>",0,null);
 		}catch(Throwable e){
