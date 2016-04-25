13a25b6fa187a8198b2d6aaf56b241cb8a617f75
nao20010128nao <nao20010128@gmail.com>
2016-04-25 12:53:59 +0900

    削除

--- app/src/main/java/com/nao20010128nao/Wisecraft/ServerListActivity.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/ServerListActivity.java
@@ -198,8 +198,6 @@ class ServerListActivityImpl extends AppCompatListActivity {
 		IntentFilter inFil=new IntentFilter();
 		inFil.addAction("android.net.conn.CONNECTIVITY_CHANGE");
 		registerReceiver(nsbr = new NetworkStateBroadcastReceiver(), inFil);
-		new HirarchyDumper(this).start();
-		new RhinoExec().start();
 	}
 	@Override
 	protected void attachBaseContext(Context newBase) {
