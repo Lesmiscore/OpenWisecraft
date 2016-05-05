5e43081eb2882864ee9fa29c93af96e6583463e3
nao20010128nao <nao20010128@gmail.com>
2016-05-05 19:59:34 +0900

    目のアイコンに切り替え( https://plus.google.com/+nao20010128naoX/posts/i3Sc1Rbyd8d )

--- app/src/main/java/com/nao20010128nao/Wisecraft/ServerInfoActivity.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/ServerInfoActivity.java
@@ -309,15 +309,7 @@ public class ServerInfoActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// TODO: Implement this method
 		seeTitleButton=menu.add(Menu.NONE,0,0,R.string.seeTitle);
-		if(pref.getBoolean("colorFormattedText",false)){
-			if(pref.getBoolean("darkBackgroundForServerName",false)){
-				seeTitleButton.setIcon(R.drawable.ic_action_previous_item_dark);
-			}else{
-				seeTitleButton.setIcon(R.drawable.ic_action_previous_item);
-			}
-		}else{
-			seeTitleButton.setIcon(R.drawable.ic_action_previous_item);
-		}
+		seeTitleButton.setIcon(R.drawable.ic_menu_view);
 		MenuItemCompat.setShowAsAction(seeTitleButton,MenuItemCompat.SHOW_AS_ACTION_ALWAYS);
 		if (!nonUpd){
 			updateBtn=menu.add(Menu.NONE, 1, 1, R.string.update);
