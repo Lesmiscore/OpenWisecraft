07c5e97a0308036045d63dcd7efb6db6b39020f0
nao20010128nao <nao20010128@gmail.com>
2016-05-08 19:33:04 +0900

    Android StudioのScrolling Activityからコード持ってきて骨抜きにした

---
+++ scrolling/src/main/java/com/nao20010128nao/TESTAPP/ScrollingActivity.java
@@ -0,0 +1,72 @@
+package com.nao20010128nao.TESTAPP;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public abstract class ScrollingActivity extends AppCompatActivity {
+	FrameLayout content;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) super.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+		content=(FrameLayout)super.findViewById(R.id.content);
+    }
+
+	@Override
+	public void setContentView(View view) {
+		// TODO: Implement this method
+		content.removeAllViews();
+		ViewGroup.LayoutParams lp=view.getLayoutParams();
+		if(lp==null)lp=tryInstantinateLayoutParams(view);
+		lp.height=lp.width=ViewGroup.LayoutParams.MATCH_PARENT;
+		view.setLayoutParams(lp);
+		content.addView(view);
+	}
+
+	@Override
+	public void setContentView(int layoutResID) {
+		// TODO: Implement this method
+		setContentView(getLayoutInflater().inflate(layoutResID,null));
+	}
+
+	@Override
+	public View findViewById(int id) {
+		// TODO: Implement this method
+		return content.findViewById(id);
+	}
+	
+	private ViewGroup.LayoutParams tryInstantinateLayoutParams(View v){
+		Class viewClass=v.getClass();
+		if(viewClass==FrameLayout.class){
+			return new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+		}
+		if(viewClass==LinearLayout.class){
+			return new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+		}
+		while(true){
+			Class lp;
+			try {
+				lp = Class.forName(viewClass.getName() + "$LayoutParams");
+			} catch (ClassNotFoundException e) {
+				viewClass=viewClass.getSuperclass();
+				continue;
+			}
+			try {
+				return (ViewGroup.LayoutParams)lp.getConstructor(int.class,int.class).newInstance(-1,-1);
+			} catch (Throwable e) {
+				
+			}
+			viewClass=viewClass.getSuperclass();
+			if(viewClass==View.class){
+				return new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+			}
+		}
+	}
+}
