e1ed8b83e8e6475e0ae0e94edf6cdf758edcb10c
nao20010128nao <nao20010128@gmail.com>
2016-05-08 15:17:36 +0900

    メソッド切り出し

--- app/src/main/java/com/nao20010128nao/Wisecraft/misc/DebugWriter.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/DebugWriter.java
@@ -6,38 +6,27 @@ import java.io.StringWriter;
 public class DebugWriter
 {
 	public static void writeToD(String tag,Throwable e){
-		StringWriter sw=new StringWriter();
-		PrintWriter pw=new PrintWriter(sw);
-		e.printStackTrace(pw);
-		pw.flush();
-		Log.d(tag,sw.toString());
+		Log.d(tag,getStacktraceAsString(e));
 	}
 	public static void writeToE(String tag,Throwable e){
-		StringWriter sw=new StringWriter();
-		PrintWriter pw=new PrintWriter(sw);
-		e.printStackTrace(pw);
-		pw.flush();
-		Log.e(tag,sw.toString());
+		Log.e(tag,getStacktraceAsString(e));
 	}
 	public static void writeToI(String tag,Throwable e){
-		StringWriter sw=new StringWriter();
-		PrintWriter pw=new PrintWriter(sw);
-		e.printStackTrace(pw);
-		pw.flush();
-		Log.i(tag,sw.toString());
+		Log.i(tag,getStacktraceAsString(e));
 	}
 	public static void writeToV(String tag,Throwable e){
-		StringWriter sw=new StringWriter();
-		PrintWriter pw=new PrintWriter(sw);
-		e.printStackTrace(pw);
-		pw.flush();
-		Log.v(tag,sw.toString());
+		Log.v(tag,getStacktraceAsString(e));
 	}
 	public static void writeToW(String tag,Throwable e){
+		Log.w(tag,getStacktraceAsString(e));
+	}
+	
+
+	private static String getStacktraceAsString(Throwable e) {
 		StringWriter sw=new StringWriter();
 		PrintWriter pw=new PrintWriter(sw);
 		e.printStackTrace(pw);
 		pw.flush();
-		Log.w(tag,sw.toString());
+		return sw.toString();
 	}
 }
