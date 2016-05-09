64c1c91bebb8673d5403ea67b093816edfea100c
nao20010128nao <nao20010128@gmail.com>
2016-05-10 07:33:36 +0900

    作成

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/AboutAppActivity.java
@@ -0,0 +1,19 @@
+package com.nao20010128nao.Wisecraft;
+import com.nao20010128nao.TESTAPP.ScrollingActivity;
+import android.os.Bundle;
+
+public class AboutAppActivity extends ScrollingActivity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about_app);
+	}
+
+	@Override
+	protected int getLayoutResId() {
+		// TODO: Implement this method
+		return super.getLayoutResId();
+	}
+}
