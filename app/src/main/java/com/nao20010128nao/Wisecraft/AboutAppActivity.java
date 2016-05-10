aa7f0f14b61a987231804a5aa472e3fe51df1d9b
nao20010128nao <nao20010128@gmail.com>
2016-05-10 21:48:57 +0900

    いろいろ

--- app/src/main/java/com/nao20010128nao/Wisecraft/AboutAppActivity.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/AboutAppActivity.java
@@ -1,11 +1,24 @@
 package com.nao20010128nao.Wisecraft;
-import com.nao20010128nao.TESTAPP.ScrollingActivity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
+import com.nao20010128nao.TESTAPP.ScrollingActivity;
 
 public class AboutAppActivity extends ScrollingActivity
 {
+	ImageView logo;
+	PopupWindow pw;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
@@ -15,11 +28,27 @@ public class AboutAppActivity extends ScrollingActivity
 		}
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about_app);
+		logo=(ImageView)getWindow().getDecorView().findViewById(R.id.pickaxe);
 	}
 
 	@Override
 	protected int getLayoutResId() {
 		// TODO: Implement this method
 		return R.layout.about_app_decor;
+	}
+	
+	public int getStatusBarHeight(){
+        final Rect rect = new Rect();
+        getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
+        return rect.top;
+    }
+	
+	public static class LogoImageViewBehavior extends CoordinatorLayout.Behavior<ImageView>{
+		boolean mIsAnimating;
+		AboutAppActivity ctx;
+		public LogoImageViewBehavior(Context context, AttributeSet attrs) {
+			super();
+			ctx=(AboutAppActivity)context;
+		}
 	}
 }
