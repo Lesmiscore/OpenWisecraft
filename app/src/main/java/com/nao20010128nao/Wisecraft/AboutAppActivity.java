17029826a256e1d9a96bd24c0e1a53b8612e4db1
nao20010128nao <nao20010128@gmail.com>
2016-05-11 13:58:01 +0900

    いろいろ追加

--- app/src/main/java/com/nao20010128nao/Wisecraft/AboutAppActivity.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/AboutAppActivity.java
@@ -11,11 +11,18 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 import com.nao20010128nao.TESTAPP.ScrollingActivity;
+import android.graphics.Typeface;
+import android.widget.Button;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 
 public class AboutAppActivity extends ScrollingActivity
 {
+	static Typeface icons;
+	
 	ImageView logo;
 	PopupWindow pw;
+	Button twitter,googleplus,youtube;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
@@ -24,6 +31,9 @@ public class AboutAppActivity extends ScrollingActivity
 			getTheme().applyStyle(R.style.AppTheme_Bright_OpenSource,true);
 		}
 		super.onCreate(savedInstanceState);
+		if(icons==null){
+			icons=Typeface.createFromAsset(getAssets(),"icomoon2.ttf");
+		}
 		setContentView(R.layout.about_app);
 		logo=(ImageView)getWindow().getDecorView().findViewById(R.id.pickaxe);
 		findViewById(R.id.gotoTranslationPage).setOnClickListener(new View.OnClickListener(){
@@ -34,6 +44,30 @@ public class AboutAppActivity extends ScrollingActivity
 				startActivity(Intent.createChooser(intent,getResources().getString(R.string.gotoTranslationPage)));
 			}
 		});
+		{
+			twitter=(Button)findViewById(R.id.twitter);
+			twitter.setTypeface(icons);
+			twitter.setTextColor(0xff1da1f2);
+			twitter.setText(String.valueOf((char)0xe906));
+		}
+		{
+			youtube=(Button)findViewById(R.id.youtube);
+			youtube.setTypeface(icons);
+			youtube.setTextColor(0xffe73128);
+			youtube.setText(String.valueOf((char)0xe900));
+			//youtube.setText(String.valueOf((char)0xe901));
+			//youtube.setText(String.valueOf((char)0xe902));
+			//youtube.setText(String.valueOf((char)0xe907));
+			//youtube.setText(String.valueOf((char)0xe908));
+		}
+		{
+			googleplus=(Button)findViewById(R.id.googleplus);
+			googleplus.setTypeface(icons);
+			googleplus.setTextColor(0xffdd5044);
+			googleplus.setText(String.valueOf((char)0xe903));
+			//googleplus.setText(String.valueOf((char)0xe904));
+			//googleplus.setText(String.valueOf((char)0xe905));
+		}
 	}
 
 	@Override
