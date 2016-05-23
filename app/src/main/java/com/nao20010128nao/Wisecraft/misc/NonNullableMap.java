9ce612ca25c6302b57d722ecc00186c12b1ef204
nao20010128nao <nao20010128@gmail.com>
2016-05-23 16:20:04 +0900

    追加

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/NonNullableMap.java
@@ -0,0 +1,30 @@
+package com.nao20010128nao.Wisecraft.misc;
+import java.util.*;
+
+public class NonNullableMap<K> extends HashMap<K,Boolean>
+{
+	public NonNullableMap() {
+		super();
+	}
+
+    public NonNullableMap(int capacity) {
+		super(capacity);
+	}
+
+    public NonNullableMap(int capacity, float loadFactor) {
+		super(capacity,loadFactor);
+	}
+
+    public NonNullableMap(Map<K,Boolean> map) {
+		super(map);
+	}
+	
+	@Override
+	public Boolean get(Object key) {
+		// TODO: Implement this method
+		Boolean b= super.get(key);
+		if (b == null)
+			return false;
+		return b;
+	}
+}
