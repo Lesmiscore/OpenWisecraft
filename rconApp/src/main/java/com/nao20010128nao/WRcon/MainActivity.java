4601839258da0a51da70aa7b5bc6f90cb21fe4aa
nao20010128nao <nao20010128@gmail.com>
2016-06-17 21:21:52 +0900

    RCONアプリ(仮)

---
+++ rconApp/src/main/java/com/nao20010128nao/WRcon/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nao20010128nao.WRcon;
+import com.nao20010128nao.Wisecraft.misc.compat.AppCompatListActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatListActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+	}
+	
+}
