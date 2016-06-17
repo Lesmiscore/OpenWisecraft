53915cefd2e3d7a7ccf467c122e7aabd2c9c35ef
nao20010128nao <nao20010128@gmail.com>
2016-06-17 22:11:48 +0900

    いろいろ

--- rconApp/src/main/java/com/nao20010128nao/WRcon/MainActivity.java
+++ rconApp/src/main/java/com/nao20010128nao/WRcon/MainActivity.java
@@ -1,14 +1,20 @@
 package com.nao20010128nao.WRcon;
 import com.nao20010128nao.Wisecraft.misc.compat.AppCompatListActivity;
 import android.os.Bundle;
+import com.nao20010128nao.Wisecraft.misc.AppBaseArrayAdapter;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatListActivity
 {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
+		setListAdapter(new ServerListAdapter());
 	}
-	
+	class ServerListAdapter extends AppBaseArrayAdapter<Server>{
+		public ServerListAdapter(){
+			super(MainActivity.this,0,new ArrayList<Server>());
+		}
+	}
 }
---
+++ rconApp/src/main/java/com/nao20010128nao/WRcon/Server.java
@@ -0,0 +1,7 @@
+package com.nao20010128nao.WRcon;
+
+public class Server
+{
+	public String ip;
+	public int port;
+}
