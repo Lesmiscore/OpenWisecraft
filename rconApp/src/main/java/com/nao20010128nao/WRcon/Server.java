7fcb4f538e932f7c6f1b890f527637a2f5a78bdf
nao20010128nao <nao20010128@gmail.com>
2016-06-18 14:19:59 +0900

    等価確認

--- rconApp/src/main/java/com/nao20010128nao/WRcon/Server.java
+++ rconApp/src/main/java/com/nao20010128nao/WRcon/Server.java
@@ -4,4 +4,28 @@ public class Server
 {
 	public String ip;
 	public int port;
+	
+
+	@Override
+	public int hashCode() {
+		// TODO: Implement this method
+		return ip.hashCode() ^ port;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		// TODO: Implement this method
+		if (!(o instanceof Server)) {
+			return false;
+		}
+		Server os=(Server)o;
+		return os.ip.equals(ip) & os.port == port;
+	}
+
+	@Override
+	public String toString() {
+		// TODO: Implement this method
+		return new StringBuilder().append(ip).append(':').append(port).toString();
+	}
+	
 }
