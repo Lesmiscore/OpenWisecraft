446032c5fa5cdc63eb9d0db9de86f79431748bfd
nao20010128nao <nao20010128@gmail.com>
2016-06-18 12:20:38 +0900

    追加

--- rconApp/src/main/java/com/nao20010128nao/WRcon/MainActivity.java
+++ rconApp/src/main/java/com/nao20010128nao/WRcon/MainActivity.java
@@ -3,6 +3,10 @@ import com.nao20010128nao.Wisecraft.misc.compat.AppCompatListActivity;
 import android.os.Bundle;
 import com.nao20010128nao.Wisecraft.misc.AppBaseArrayAdapter;
 import java.util.ArrayList;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.Adapter;
+import android.view.View;
 
 public class MainActivity extends AppCompatListActivity
 {
@@ -10,11 +14,26 @@ public class MainActivity extends AppCompatListActivity
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
-		setListAdapter(new ServerListAdapter());
+		ServerListAdapter sla=new ServerListAdapter();
+		setListAdapter(sla);
+		getListView().setLongClickable(true);
+		getListView().setOnItemClickListener(sla);
+		getListView().setOnItemLongClickListener(sla);
 	}
-	class ServerListAdapter extends AppBaseArrayAdapter<Server>{
+	class ServerListAdapter extends AppBaseArrayAdapter<Server> implements ListView.OnItemClickListener,ListView.OnItemLongClickListener{
 		public ServerListAdapter(){
 			super(MainActivity.this,0,new ArrayList<Server>());
+		}
+
+		@Override
+		public void onItemClick(AdapterView<?> p1, View p2, int p3, long p4) {
+			// TODO: Implement this method
+		}
+
+		@Override
+		public boolean onItemLongClick(AdapterView<?> p1, View p2, int p3, long p4) {
+			// TODO: Implement this method
+			return false;
 		}
 	}
 }
