b33f23bbdc1fb8347713bb90461aa27c74a017a2
nao20010128nao <nao20010128@gmail.com>
2016-06-19 15:22:25 +0900

    サーバーを追加

--- rconApp/src/main/java/com/nao20010128nao/WRcon/MainActivity.java
+++ rconApp/src/main/java/com/nao20010128nao/WRcon/MainActivity.java
@@ -15,15 +15,22 @@ import android.widget.TextView;
 import android.view.Menu;
 import android.view.SubMenu;
 import android.support.v4.view.MenuItemCompat;
+import android.view.MenuItem;
+import com.nao20010128nao.Wisecraft.misc.compat.AppCompatAlertDialog;
+import android.content.DialogInterface;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.support.design.widget.Snackbar;
 
 public class MainActivity extends AppCompatListActivity
 {
 	List<Server> list;
+	ServerListAdapter sla;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
-		ServerListAdapter sla=new ServerListAdapter();
+		sla=new ServerListAdapter();
 		setListAdapter(sla);
 		getListView().setLongClickable(true);
 		getListView().setOnItemClickListener(sla);
@@ -33,11 +40,47 @@ public class MainActivity extends AppCompatListActivity
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// TODO: Implement this method
-		SubMenu add=menu.addSubMenu(Menu.NONE,1,0,R.string.add).setIcon(R.drawable.ic_action_new_light);
+		SubMenu add=menu.addSubMenu(Menu.NONE,0,1,R.string.add).setIcon(R.drawable.ic_action_new_light);
 		add.add(Menu.NONE,1,1,R.string.addSingle).setIcon(R.drawable.ic_action_new_light);
-		add.add(Menu.NONE,1,2,R.string.importFromWc).setIcon(R.drawable.ic_action_import_export_light);
+		add.add(Menu.NONE,2,1,R.string.importFromWc).setIcon(R.drawable.ic_action_import_export_light);
 		MenuItemCompat.setShowAsAction(add.getItem(),MenuItemCompat.SHOW_AS_ACTION_IF_ROOM);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO: Implement this method
+		switch(item.getItemId()){
+			case 1:
+				final View dialogView=getLayoutInflater().inflate(R.layout.server_add_dialog_new,null);
+				new AppCompatAlertDialog.Builder(this,R.style.AppAlertDialog)
+					.setView(dialogView)
+					.setTitle(R.string.add)
+					.setPositiveButton(android.R.string.ok,new DialogInterface.OnClickListener(){
+						public void onClick(DialogInterface di,int w){
+							String ip=((EditText)dialogView.findViewById(R.id.serverIp)).getText().toString();
+							String portStr=((EditText)dialogView.findViewById(R.id.serverPort)).getText().toString();
+							int port;
+							try{
+								port=Integer.valueOf(portStr);
+							}catch(Throwable e){
+								Snackbar.make(findViewById(android.R.id.content),R.string.numError,Snackbar.LENGTH_LONG).show();
+								return;
+							}
+							Server sv=new Server();
+							sv.ip=ip;
+							sv.port=port;
+							sla.add(sv);
+						}
+					})
+					.setNegativeButton(android.R.string.cancel,null)
+					.show();
+				break;
+			case 2:
+				
+				break;
+		}
+		return super.onOptionsItemSelected(item)|true;
 	}
 	
 	
@@ -62,7 +105,7 @@ public class MainActivity extends AppCompatListActivity
 			// TODO: Implement this method
 			if(convertView==null)convertView=getLayoutInflater().inflate(R.layout.main_server_entry,null);
 			((TextView)convertView.findViewById(R.id.serverIp)).setText(getItem(position).toString());
-			return super.getView(position, convertView, parent);
+			return convertView;
 		}
 		
 
