de71cec157443dc894a94044db62dacd519eb17e
nao20010128nao <nao20010128@gmail.com>
2016-06-21 14:33:32 +0900

    追加

--- rconApp/src/main/java/com/nao20010128nao/WRcon/AboutAppActivity.java
+++ rconApp/src/main/java/com/nao20010128nao/WRcon/AboutAppActivity.java
@@ -1,6 +1,9 @@
 package com.nao20010128nao.WRcon;
 import com.nao20010128nao.Wisecraft.AboutAppActivityBase;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.net.Uri;
 
 public class AboutAppActivity extends AboutAppActivityBase
 {
@@ -9,5 +12,14 @@ public class AboutAppActivity extends AboutAppActivityBase
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
+		addCardAt(R.layout.about_app_2_local,0);
+		findViewById(R.id.gotoTranslationPage).setOnClickListener(new View.OnClickListener(){
+				public void onClick(View v){
+					Intent intent=new Intent(Intent.ACTION_VIEW);
+					intent.addCategory(Intent.CATEGORY_DEFAULT);
+					intent.setData(Uri.parse(getResources().getString(R.string.aboutAppTranslationUrl)));
+					startActivity(Intent.createChooser(intent,getResources().getString(R.string.gotoTranslationPage)));
+				}
+			});
 	}
 }
