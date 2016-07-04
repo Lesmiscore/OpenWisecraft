9e7857730822433ef4b67bbde5960c9079fdc8ca
nao20010128nao <nao20010128@gmail.com>
2016-07-04 15:29:43 +0900

    修正

--- aFileChooser/src/main/java/com/ipaulpro/afilechooser/FileOpenChooserActivity.java
+++ aFileChooser/src/main/java/com/ipaulpro/afilechooser/FileOpenChooserActivity.java
@@ -10,9 +10,9 @@ public class FileOpenChooserActivity extends FileChooserActivity
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// TODO: Implement this method
-		SubMenu addFile=menu.addSubMenu(Menu.NONE,0,3,R.string.create).setIcon(getPresenter().isLightTheme(this)?R.drawable.ic_action_new_light:R.drawable.ic_action_new_dark);
-		addFile.add(Menu.NONE,0,4,R.string.add_file).setIcon(getTintedDrawable(R.drawable.ic_description_black_48dp,getPresenter().isLightTheme(this)?0xff_666666:0xff_ffffff));
-		addFile.add(Menu.NONE,0,5,R.string.add_dir).setIcon(getPresenter().isLightTheme(this)?R.drawable.ic_action_collection_light:R.drawable.ic_action_collection_dark);
+		SubMenu addFile=menu.addSubMenu(0,4,0,R.string.create).setIcon(getPresenter().isLightTheme(this)?R.drawable.ic_action_new_light:R.drawable.ic_action_new_dark);
+		addFile.add(0,5,1,R.string.add_file).setIcon(getTintedDrawable(R.drawable.ic_description_black_48dp,getPresenter().isLightTheme(this)?0xff_666666:0xff_ffffff));
+		addFile.add(0,6,2,R.string.add_dir).setIcon(getPresenter().isLightTheme(this)?R.drawable.ic_action_collection_light:R.drawable.ic_action_collection_dark);
 		MenuItemCompat.setShowAsAction(addFile.getItem(),MenuItem.SHOW_AS_ACTION_ALWAYS);
 		return super.onCreateOptionsMenu(menu);
 	}
