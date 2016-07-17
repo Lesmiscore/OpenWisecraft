c4ea5f821f1e4f56a592353715a7a42e5262f05d
nao20010128nao <nao20010128@gmail.com>
2016-07-17 11:53:09 +0900

    実装

--- app/src/main/java/com/nao20010128nao/Wisecraft/misc/skin_face/CrafaterSkinFetcher.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/skin_face/CrafaterSkinFetcher.java
@@ -1,5 +1,37 @@
 package com.nao20010128nao.Wisecraft.misc.skin_face;
+import java.net.URL;
+import java.net.MalformedURLException;
+import com.nao20010128nao.Wisecraft.misc.DebugWriter;
+import android.graphics.Bitmap;
 
 public class CrafaterSkinFetcher extends SkinFaceFetcher
 {
+	ImageLoader il=new ImageLoader();
+	public void requestLoadSkin(String player,SkinFetcher.SkinFetchListener listener){
+		try {
+			il.putInQueue(new URL("https://crafatar.com/avatars/"+player+"?overlay=true&size=1"), new LoaderListener(player, listener));
+		} catch (MalformedURLException e) {
+			DebugWriter.writeToE("SkinFetcher",e);
+		}
+	}
+	class LoaderListener implements ImageLoader.ImageStatusListener {
+		String player;
+		SkinFetcher.SkinFetchListener lis;
+		public LoaderListener(String name,SkinFetcher.SkinFetchListener lis){
+			player=name;
+			this.lis=lis;
+		}
+		@Override
+		public void onError(Throwable err, URL url) {
+			// TODO: Implement this method
+			lis.onError(player);
+		}
+
+		@Override
+		public void onSuccess(Bitmap bmp, URL url) {
+			// TODO: Implement this method
+			lis.onSuccess(bmp,player);
+		}
+
+	}
 }
