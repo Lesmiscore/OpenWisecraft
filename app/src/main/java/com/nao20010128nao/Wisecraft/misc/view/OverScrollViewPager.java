95b60733386d1a078a051bbe4689f824fa1c3d0c
nao20010128nao <nao20010128@gmail.com>
2016-07-18 16:58:19 +0900

    オーバースクロール用のViewPagerに置き換え http://stackoverflow.com/questions/13346824/viewpager-detect-when-user-is-trying-to-swipe-out-of-bounds

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/view/OverScrollViewPager.java
@@ -0,0 +1,53 @@
+package com.nao20010128nao.Wisecraft.misc.view;
+import android.support.v4.view.ViewPager;
+import android.view.MotionEvent;
+import android.content.Context;
+import android.util.AttributeSet;
+
+
+/*
+ http://stackoverflow.com/questions/13346824/viewpager-detect-when-user-is-trying-to-swipe-out-of-bounds
+*/
+public class OverScrollViewPager extends ViewPager {
+
+    float mStartDragX;
+    OnSwipeOutListener mListener;
+
+
+	public OverScrollViewPager(Context ctx){
+		super(ctx);
+	}
+	
+    public OverScrollViewPager(Context ctx,AttributeSet as){
+		super(ctx,as);
+	}
+
+    public void setOnSwipeOutListener(OnSwipeOutListener listener) {
+        mListener = listener;
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        float x = ev.getX();
+        switch (ev.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				mStartDragX = x;
+				break;
+			case MotionEvent.ACTION_MOVE:
+				if (mStartDragX < x && getCurrentItem() == 0) {
+					if(mListener!=null)mListener.onSwipeOutAtStart();
+				} else if (mStartDragX > x && getCurrentItem() == getAdapter().getCount() - 1) {
+					if(mListener!=null)mListener.onSwipeOutAtEnd();
+				}
+				break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    public interface OnSwipeOutListener {
+        public void onSwipeOutAtStart();
+        public void onSwipeOutAtEnd();
+    }
+
+}
