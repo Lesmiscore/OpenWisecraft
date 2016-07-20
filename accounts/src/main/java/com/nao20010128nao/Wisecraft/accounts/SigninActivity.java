f6db8f68289e110261f033d651ed8203a4d4d6ce
nao20010128nao <nao20010128@gmail.com>
2016-07-20 12:44:11 +0900

    制作途中

---
+++ accounts/src/main/java/com/nao20010128nao/Wisecraft/accounts/SigninActivity.java
@@ -0,0 +1,15 @@
+package com.nao20010128nao.Wisecraft.accounts;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SigninActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.accounts_signin_screen);
+	}
+	
+}
