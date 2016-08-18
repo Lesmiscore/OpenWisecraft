f40e15caf3ab3f06d627b3997dec52aaa46e72c7
nao20010128nao <nao20010128@gmail.com>
2016-08-18 23:15:06 +0900

    修正

--- app/src/main/java/com/nao20010128nao/Wisecraft/FragmentSettingsActivity.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/FragmentSettingsActivity.java
@@ -573,7 +573,7 @@ public class FragmentSettingsActivity extends AppCompatActivity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 			// TODO: Implement this method
-			return LayoutInflater.from(getActivity()).inflate(R.layout.fragment_settings_preview_server_info_toolbar,container,false);
+			return LayoutInflater.from(getActivity()).inflate(R.layout.server_info_pager,container,false);
 		}
 		
 		public static class BlankFragment extends com.nao20010128nao.Wisecraft.misc.BaseFragment<FragmentSettingsActivity> {
