609ec95151029f8566b902cb894b359256785d61
nao20010128nao <nao20010128@gmail.com>
2016-08-21 21:43:13 +0900

    所属を変更

---
+++ compat/src/main/java/com/nao20010128nao/Wisecraft/VersionInfoFragment.java
@@ -0,0 +1,13 @@
+package com.nao20010128nao.Wisecraft;
+import android.os.*;
+import android.support.v7.preference.*;
+import android.support.v7.widget.*;
+import android.view.*;
+
+public class VersionInfoFragment extends PreferenceFragmentCompat
+{
+	@Override
+	public void onCreatePreferences(Bundle p1, String p2) {
+		// TODO: Implement this method
+	}
+}
