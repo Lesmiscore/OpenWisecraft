83aadce3b20acadc824e8d1656b32dfb0d0c4dd4
nao20010128nao <nao20010128@gmail.com>
2016-09-05 21:03:35 +0900

    追加

--- app/src/main/java/com/nao20010128nao/Wisecraft/AboutAppActivity.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/AboutAppActivity.java
@@ -20,6 +20,7 @@ public class AboutAppActivity extends AboutAppActivityBase
 		addCardAt(R.layout.about_app_2,0);//234
 		addCardAt(R.layout.about_app_1,0);//1234
 		addCard(R.layout.about_app_5);//12345
+        addCard(R.layout.about_app_6);//123456
 		findViewById(R.id.gotoTranslationPage).setOnClickListener(new View.OnClickListener(){
 				public void onClick(View v){
 					Intent intent=new Intent(Intent.ACTION_VIEW);
@@ -35,6 +36,14 @@ public class AboutAppActivity extends AboutAppActivityBase
 					intent.setData(Uri.parse(getResources().getString(R.string.bugReportUrl)));
 					startActivity(Intent.createChooser(intent,getResources().getString(R.string.gotoBugReport)));
 				}
+			});
+        findViewById(R.id.gotoWisecraftTools).setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    Intent intent=new Intent(Intent.ACTION_VIEW);
+                    intent.addCategory(Intent.CATEGORY_DEFAULT);
+                    intent.setData(Uri.parse(getResources().getString(R.string.wisecraftToolsUrl)));
+                    startActivity(Intent.createChooser(intent,getResources().getString(R.string.gotoWisecraftTools)));
+                }
 			});
 	}
 }
