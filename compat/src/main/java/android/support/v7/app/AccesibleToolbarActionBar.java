c987b5a164103e0c6148f7402bb1fcd5c414e248
nao20010128nao <nao20010128@gmail.com>
2016-09-09 21:42:55 +0900

    サーバー詳細画面のFragment化する魔改造中 終わってません

---
+++ compat/src/main/java/android/support/v7/app/AccesibleToolbarActionBar.java
@@ -0,0 +1,11 @@
+package android.support.v7.app;
+
+import android.support.v7.widget.*;
+import android.view.*;
+
+public class AccesibleToolbarActionBar extends ToolbarActionBar 
+{
+	public AccesibleToolbarActionBar(Toolbar toolbar, CharSequence title, Window.Callback callback) {
+        super(toolbar,title,callback);
+    }
+}
