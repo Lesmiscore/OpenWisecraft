b931257620f5c848b1608d3b92b50fcfce458a90
nao20010128nao <nao20010128@gmail.com>
2016-09-13 07:34:02 +0900

    修正

--- ci/git.sh
+++ ci/git.sh
@@ -2,22 +2,22 @@ git config --global user.email "nao20010128@gmail.com"
 git config --global user.name "nao20010128nao"
 git submodule update --init --recursive
 cd wisecraft-i18n
-git pull origin master > NUL
-git checkout master > NUL
+git pull origin master > /dev/null
+git checkout master > /dev/null
 cd ../statusesLayout
-git pull origin master > NUL
-git checkout master > NUL
+git pull origin master > /dev/null
+git checkout master > /dev/null
 cd ../MaterialIcons
-git pull origin master > NUL
-git checkout master > NUL
+git pull origin master > /dev/null
+git checkout master > /dev/null
 cd ../calligraphy
-git pull origin master > NUL
-git checkout master > NUL
+git pull origin master > /dev/null
+git checkout master > /dev/null
 cd ../psts
-git pull origin master > NUL
-git checkout master > NUL
+git pull origin master > /dev/null
+git checkout master > /dev/null
 cd ../colorPicker
-git pull origin master > NUL
-git checkout master > NUL
+git pull origin master > /dev/null
+git checkout master > /dev/null
 cd ../
   
