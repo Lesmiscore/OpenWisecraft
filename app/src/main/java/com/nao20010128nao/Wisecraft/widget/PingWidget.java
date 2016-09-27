d99933295de26725f5120b78f229df7c7d20e7ae
nao20010128nao <nao20010128@gmail.com>
2016-09-28 01:42:20 +0900

    大体完成?

--- app/src/main/java/com/nao20010128nao/Wisecraft/widget/PingWidget.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/widget/PingWidget.java
@@ -2,11 +2,20 @@ package com.nao20010128nao.Wisecraft.widget;
 
 import android.appwidget.*;
 import android.content.*;
+import android.graphics.*;
+import android.support.v4.content.*;
 import android.widget.*;
 import com.google.gson.*;
 import com.nao20010128nao.Wisecraft.*;
 import com.nao20010128nao.Wisecraft.misc.*;
+import com.nao20010128nao.Wisecraft.misc.pinger.pc.*;
+import com.nao20010128nao.Wisecraft.misc.pinger.pe.*;
 import com.nao20010128nao.Wisecraft.misc.provider.*;
+import java.util.*;
+
+import static com.nao20010128nao.Wisecraft.misc.Utils.*;
+import android.preference.*;
+import android.app.*;
 
 public class PingWidget extends AppWidgetProvider 
 {
@@ -51,50 +60,156 @@ public class PingWidget extends AppWidgetProvider
 		}
 		NormalServerPingProvider nspp=new NormalServerPingProvider();
 		for(int wid:appWidgetIds){
-			if(widgetPref.contains(wid+"")){
-				//show activity here
+			if(!widgetPref.contains(wid+"")){
+				context.startActivity(new Intent(context,WidgetServerSelectActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).putExtra("wid",wid));
 				continue;
 			}
 			Server s=gson.fromJson(widgetPref.getString(wid+"","{}"),Server.class);
 			NonBrodRevPingHandler ph=new NonBrodRevPingHandler();
 			ph.id=wid;
 			ph.c=context;
+			ph.awm=appWidgetManager;
 			nspp.putInQueue(s,ph);
+			ServerStatusRemoteViewsWrapper viewHolder=new ServerStatusRemoteViewsWrapper(context);
+			RemoteViews rvs=(RemoteViews)viewHolder.getTag();
+			viewHolder.pending(s,context);
+			rvs.setOnClickPendingIntent(R.id.update,PendingIntent.getBroadcast(context,0,new Intent(context,PingHandler.class).putExtra("wid",wid),0));
+			appWidgetManager.updateAppWidget(wid,rvs);
 		}
-		//appWidgetManager.updateAppWidget();
+	}
+
+	@Override
+	public void onDeleted(Context context, int[] appWidgetIds) {
+		SharedPreferences widgetPref=context.getSharedPreferences("widgets",Context.MODE_PRIVATE);
+		SharedPreferences.Editor edt=widgetPref.edit();
+		for(int i:appWidgetIds)edt.remove(i+"");
+		edt.commit();
+	}
+
+	@Override
+	public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
+		SharedPreferences widgetPref=context.getSharedPreferences("widgets",Context.MODE_PRIVATE);
+		Map<String,Object> datas=new HashMap<>(widgetPref.getAll());
+		SharedPreferences.Editor edt=widgetPref.edit();
+		for(int i=0;i<oldWidgetIds.length;i++){
+			edt.remove(oldWidgetIds[i]+"")
+				.putString(newWidgetIds[i]+"",datas.get(oldWidgetIds[i]+"")+"");
+		}
+		edt.commit();
 	}
 	
-	public static class PingHandler extends BroadcastReceiver /*implements ServerPingProvider.PingHandler*/ {
+	
+	
+	public static class PingHandler extends BroadcastReceiver {
 
 		@Override
 		public void onReceive(Context p1, Intent p2) {
-			// TODO: Implement this method
+			int wid=p2.getIntExtra("wid",0);
+			SharedPreferences widgetPref=p1.getSharedPreferences("widgets",Context.MODE_PRIVATE);
+			Gson gson=new Gson();
+			Server s=gson.fromJson(widgetPref.getString(wid+"","{}"),Server.class);
+			NormalServerPingProvider nspp=new NormalServerPingProvider();
+			NonBrodRevPingHandler ph=new NonBrodRevPingHandler();
+			ph.id=wid;
+			ph.c=p1;
+			ph.awm=AppWidgetManager.getInstance(p1);
+			ServerStatusRemoteViewsWrapper viewHolder=new ServerStatusRemoteViewsWrapper(p1);
+			RemoteViews rvs=(RemoteViews)viewHolder.getTag();
+			viewHolder.pending(s,p1);
+			rvs.setOnClickPendingIntent(R.id.update,PendingIntent.getBroadcast(p1,0,new Intent(p1,PingHandler.class).putExtra("wid",wid),0));
+			ph.awm.updateAppWidget(wid,rvs);
+			nspp.putInQueue(s,ph);
 		}
-
-		/*
-		@Override
-		public void onPingArrives(ServerStatus stat) {
-			// TODO: Implement this method
-		}
-
-		@Override
-		public void onPingFailed(Server server) {
-			// TODO: Implement this method
-		}
-		*/
 	}
 	
-	class NonBrodRevPingHandler implements ServerPingProvider.PingHandler{
-		int id;Context c;
+	static class NonBrodRevPingHandler implements ServerPingProvider.PingHandler{
+		int id;Context c;AppWidgetManager awm;
 		@Override
-		public void onPingArrives(ServerStatus stat) {
-			ServerStatusRemoteViewsWrapper ssrvw=new ServerStatusRemoteViewsWrapper(c);
-			RemoteViews rvs=(RemoteViews)ssrvw.getTag();
+		public void onPingArrives(ServerStatus s) {
+			SharedPreferences pref=PreferenceManager.getDefaultSharedPreferences(c);
+			
+			ServerStatusRemoteViewsWrapper viewHolder=new ServerStatusRemoteViewsWrapper(c);
+			RemoteViews rvs=(RemoteViews)viewHolder.getTag();
+			
+			viewHolder.setStatColor(ContextCompat.getColor(c, R.color.stat_ok));
+			final String title;
+			if (s.response instanceof FullStat) {//PE
+				FullStat fs = (FullStat) s.response;
+				Map<String, String> m = fs.getDataAsMap();
+				if (m.containsKey("hostname")) {
+					title = m.get("hostname");
+				} else if (m.containsKey("motd")) {
+					title = m.get("motd");
+				} else {
+					title = s.toString();
+				}
+				viewHolder.setServerPlayers(m.get("numplayers"), m.get("maxplayers"));
+			} else if (s.response instanceof Reply19) {//PC 1.9~
+				Reply19 rep = (Reply19) s.response;
+				if (rep.description == null) {
+					title = s.toString();
+				} else {
+					title = rep.description.text;
+				}
+				viewHolder.setServerPlayers(rep.players.online, rep.players.max);
+			} else if (s.response instanceof Reply) {//PC
+				Reply rep = (Reply) s.response;
+				if (rep.description == null) {
+					title = s.toString();
+				} else {
+					title = rep.description;
+				}
+				viewHolder.setServerPlayers(rep.players.online, rep.players.max);
+			} else if (s.response instanceof SprPair) {//PE?
+				SprPair sp = ((SprPair) s.response);
+				if (sp.getB() instanceof UnconnectedPing.UnconnectedPingResult) {
+					UnconnectedPing.UnconnectedPingResult res = (UnconnectedPing.UnconnectedPingResult) sp.getB();
+					title = res.getServerName();
+					viewHolder.setServerPlayers(res.getPlayersCount(), res.getMaxPlayers());
+				} else if (sp.getA() instanceof FullStat) {
+					FullStat fs = (FullStat) sp.getA();
+					Map<String, String> m = fs.getDataAsMap();
+					if (m.containsKey("hostname")) {
+						title = m.get("hostname");
+					} else if (m.containsKey("motd")) {
+						title = m.get("motd");
+					} else {
+						title = s.toString();
+					}
+					viewHolder.setServerPlayers(m.get("numplayers"), m.get("maxplayers"));
+				} else {
+					title = s.toString();
+					viewHolder.setServerPlayers();
+				}
+			} else if (s.response instanceof UnconnectedPing.UnconnectedPingResult) {//PE
+				UnconnectedPing.UnconnectedPingResult res = (UnconnectedPing.UnconnectedPingResult) s.response;
+				title = res.getServerName();
+				viewHolder.setServerPlayers(res.getPlayersCount(), res.getMaxPlayers());
+			} else {//Unreachable
+				title = s.toString();
+				viewHolder.setServerPlayers();
+			}
+			if (pref.getBoolean("serverListColorFormattedText", false)) {
+				viewHolder.setServerName(parseMinecraftFormattingCode(title,Color.WHITE));
+			} else {
+				viewHolder.setServerName(deleteDecorations(title));
+			}
+			viewHolder
+				.setPingMillis(s.ping)
+				.setServer(s);
+			
+			rvs.setOnClickPendingIntent(R.id.update,PendingIntent.getBroadcast(c,0,new Intent(c,PingHandler.class).putExtra("wid",id),0));
+			awm.updateAppWidget(id,rvs);
 		}
 
 		@Override
 		public void onPingFailed(Server server) {
+			ServerStatusRemoteViewsWrapper viewHolder=new ServerStatusRemoteViewsWrapper(c);
+			RemoteViews rvs=(RemoteViews)viewHolder.getTag();
+			viewHolder.offline(server,c);
 			
+			rvs.setOnClickPendingIntent(R.id.update,PendingIntent.getBroadcast(c,0,new Intent(c,PingHandler.class).putExtra("wid",id),0));
+			awm.updateAppWidget(id,rvs);
 		}
 	}
 }
---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/widget/WidgetServerSelectActivity.java
@@ -0,0 +1,67 @@
+package com.nao20010128nao.Wisecraft.widget;
+
+import android.content.*;
+import android.os.*;
+import android.support.v7.app.*;
+import android.support.v7.widget.*;
+import android.view.*;
+import android.widget.*;
+import com.google.gson.*;
+import com.nao20010128nao.Wisecraft.*;
+import com.nao20010128nao.Wisecraft.misc.*;
+import android.preference.*;
+import android.content.res.*;
+import android.view.View.*;
+
+public class WidgetServerSelectActivity extends AppCompatActivity 
+{
+	RecyclerView rv;
+	Adapter a;
+	Gson gson=new Gson();
+	SharedPreferences pref,widgetPref;
+	int wid;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.recycler_view_content);
+		pref=PreferenceManager.getDefaultSharedPreferences(this);
+		rv=(RecyclerView)findViewById(android.R.id.list);
+		wid=getIntent().getIntExtra("wid",0);
+		widgetPref=getSharedPreferences("widgets",Context.MODE_PRIVATE);
+		//We won't apply any style here, because this is only to select.
+		rv.setLayoutManager(new LinearLayoutManager(this));
+		rv.setAdapter(a=new Adapter());
+		Server[] servers=gson.fromJson(pref.getString("servers","[]"),Server[].class);
+		a.addAll(servers);
+	}
+	
+	class Adapter extends ListRecyclerViewAdapter<FindableViewHolder,Server> {
+
+		@Override
+		public void onBindViewHolder(FindableViewHolder parent, int offset) {
+			((TextView)parent.findViewById(android.R.id.text1)).setText(getItem(offset).toString());
+			TypedArray ta=obtainStyledAttributes(new int[]{R.attr.selectableItemBackground});
+			parent.itemView.setBackground(ta.getDrawable(0));
+			ta.recycle();
+			parent.itemView.setTag(getItem(offset));
+			Utils.applyHandlersForViewTree(parent.itemView,new OnClickListener(offset));
+		}
+
+		@Override
+		public FindableViewHolder onCreateViewHolder(ViewGroup parent, int type) {
+			return new FindableViewHolder(getLayoutInflater().inflate(android.R.layout.simple_list_item_1,parent,false));
+		}
+		
+		class OnClickListener implements View.OnClickListener{
+			int ofs;
+			public OnClickListener(){}
+			public OnClickListener(int i){ofs=i;}
+			@Override
+			public void onClick(View p1) {
+				widgetPref.edit().putString(wid+"",gson.toJson(getItem(ofs))).commit();
+				finish();
+			}
+		}
+	}
+}
