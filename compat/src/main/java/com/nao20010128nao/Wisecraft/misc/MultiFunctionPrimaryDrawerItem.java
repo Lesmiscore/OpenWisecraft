6a5971fb2850df1e7be0deb81d6ae5d8044f4b3f
nao20010128nao <nao20010128@gmail.com>
2016-10-10 10:52:02 +0900

    修正

--- compat/src/main/java/com/nao20010128nao/Wisecraft/misc/MultiFunctionPrimaryDrawerItem.java
+++ compat/src/main/java/com/nao20010128nao/Wisecraft/misc/MultiFunctionPrimaryDrawerItem.java
@@ -1,15 +1,41 @@
 package com.nao20010128nao.Wisecraft.misc;
+import android.content.*;
 import android.view.*;
 import android.widget.*;
 import com.mikepenz.materialdrawer.*;
+import com.mikepenz.materialdrawer.holder.*;
 import com.mikepenz.materialdrawer.model.interfaces.*;
 import com.nao20010128nao.Wisecraft.misc.compat.*;
+import java.util.*;
 
 import com.nao20010128nao.Wisecraft.misc.compat.R;
 
 public class MultiFunctionPrimaryDrawerItem extends LineWrappingPrimaryDrawerItem implements View.OnClickListener
 {
 	Drawer.OnDrawerItemClickListener clickListener;
+	
+    @Override
+    public void bindView(ViewHolder viewHolder, List payloads) {
+        Context ctx = viewHolder.itemView.getContext();
+		View badgeContainer;
+		TextView badge;
+		badgeContainer = viewHolder.itemView.findViewById(R.id.material_drawer_badge_container);
+		badge = (TextView) viewHolder.itemView.findViewById(R.id.material_drawer_badge);
+        bindViewHelper(new ViewHolder(viewHolder.itemView.findViewById(R.id.main)));
+        boolean badgeVisible = StringHolder.applyToOrHide(mBadge, badge);
+        if (badgeVisible) {
+            mBadgeStyle.style(badge, getTextColorStateList(getColor(ctx), getSelectedTextColor(ctx)));
+            badgeContainer.setVisibility(View.VISIBLE);
+        } else {
+            badgeContainer.setVisibility(View.GONE);
+        }
+        if (getTypeface() != null) {
+            badge.setTypeface(getTypeface());
+        }
+
+    	onPostBindView(this, viewHolder.itemView);
+    }
+	
 	@Override
 	public void onPostBindView(IDrawerItem drawerItem, View view) {
 		super.onPostBindView(drawerItem, view);
