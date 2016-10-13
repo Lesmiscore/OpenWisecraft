7f825d03042f37785aaf7b7af07dd6c6e994fbc8
nao20010128nao <nao20010128@gmail.com>
2016-10-13 16:42:26 +0900

    追加

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/provider/TcpMultiServerPingProvider.java
@@ -0,0 +1,61 @@
+package com.nao20010128nao.Wisecraft.misc.provider;
+
+import com.nao20010128nao.Wisecraft.misc.*;
+import java.util.*;
+
+public class TcpMultiServerPingProvider implements ServerPingProvider {
+    List<TcpServerPingProvider> objects=new ArrayList<>();
+    int count=0;
+    public TcpMultiServerPingProvider(String head,int port,int parallels) {
+        for (int i=0;i < parallels;i++) {
+            objects.add(new TcpServerPingProvider(head,port));
+        }
+    }
+    @Override
+    public void putInQueue(Server server, ServerPingProvider.PingHandler handler) {
+        // TODO: Implement this method
+        objects.get(count).putInQueue(server, handler);
+        count++;
+        count = count % objects.size();
+    }
+
+    @Override
+    public int getQueueRemain() {
+        // TODO: Implement this method
+        int i=0;
+        for (ServerPingProvider spp:objects) {
+            i += spp.getQueueRemain();
+        }
+        return i;
+    }
+    @Override
+    public void stop() {
+        // TODO: Implement this method
+        for (ServerPingProvider spp:objects) {
+            spp.stop();
+        }
+    }
+    @Override
+    public void clearQueue() {
+        // TODO: Implement this method
+        for (ServerPingProvider spp:objects) {
+            spp.clearQueue();
+        }
+    }
+
+    @Override
+    public void offline() {
+        // TODO: Implement this method
+        for (ServerPingProvider spp:objects) {
+            spp.offline();
+        }
+    }
+
+    @Override
+    public void online() {
+        // TODO: Implement this method
+        for (ServerPingProvider spp:objects) {
+            spp.online();
+        }
+    }
+}
---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/provider/TcpServerPingProvider.java
@@ -0,0 +1,124 @@
+package com.nao20010128nao.Wisecraft.misc.provider;
+import android.text.*;
+import android.util.*;
+import com.nao20010128nao.Wisecraft.misc.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import com.nao20010128nao.Wisecraft.misc.pinger.*;
+import com.nao20010128nao.Wisecraft.*;
+
+public class TcpServerPingProvider implements ServerPingProvider
+{
+    String host;int port;
+    boolean offline;
+    Queue<Map.Entry<Server,PingHandler>> queue=new LinkedList<>();
+	Thread pingThread=new PingThread();
+    
+    public TcpServerPingProvider(String host,int port){
+        if(TextUtils.isEmpty(host))throw new IllegalArgumentException("host");
+		if(port<1|port>65535)throw new IllegalArgumentException("port");
+        this.host=host;
+		this.port=port;
+    }
+    
+    public void putInQueue(Server server, PingHandler handler) {
+        Utils.requireNonNull(server);
+        Utils.requireNonNull(handler);
+        queue.add(new KVP<Server,PingHandler>(server, handler));
+        if (!pingThread.isAlive()) {
+            pingThread = new PingThread();
+            pingThread.start();
+        }
+    }
+    @Override
+    public int getQueueRemain() {
+        // TODO: Implement this method
+        return queue.size();
+    }
+    @Override
+    public void stop() {
+        // TODO: Implement this method
+        pingThread.interrupt();
+    }
+
+    @Override
+    public void clearQueue() {
+        // TODO: Implement this method
+        queue.clear();
+    }
+
+    @Override
+    public void offline() {
+        // TODO: Implement this method
+        offline=true;
+    }
+
+    @Override
+    public void online() {
+        // TODO: Implement this method
+        offline=false;
+    }
+
+
+    private class PingThread extends Thread implements Runnable {
+        @Override
+        public void run() {
+            // TODO: Implement this method
+            Map.Entry<Server,PingHandler> now=null;
+            while (!(queue.isEmpty()|isInterrupted())) {
+                Log.d("HSPP", "Starting ping");
+                try {
+                    now = queue.poll();
+                    if(offline){
+                        Log.d("TSPP", "Offline");
+                        try {
+                            now.getValue().onPingFailed(now.getKey());
+                        } catch (Throwable ex_) {
+
+                        }
+                        continue;
+                    }
+                    try {
+                        ServerStatus stat=null;
+                        Server s=now.getKey();
+						Socket sock=null;
+                        InputStream is=null;
+                        try{
+							sock=new Socket(host,port);
+							DataOutputStream dos=new DataOutputStream(sock.getOutputStream());
+							dos.writeUTF(s.ip);
+							dos.writeInt(s.port);
+							dos.writeInt(s.mode);
+							dos.flush();
+                            is=new BufferedInputStream(sock.getInputStream());
+                            stat = PingSerializeProvider.loadFromServerDumpFile(is);
+                        }finally{
+							try {
+								if (sock != null)sock.close();
+								if (is != null)is.close();
+							} catch (Throwable e) {
+								WisecraftError.report("TcpServerPingProvider",e);
+							}
+                        }
+                        try {
+                            now.getValue().onPingArrives(stat);
+                        } catch (Throwable f) {
+
+                        }
+                    } catch (Throwable e) {
+                        WisecraftError.report("TcpServerPingProvider",e);
+                        try {
+                            now.getValue().onPingFailed(now.getKey());
+                        } catch (Throwable ex_) {
+
+                        }
+                    }
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+                Log.d("HSPP", "Next");
+            }
+        }
+	}
+}
