a7fdc83b90c33a3e036c09097dc7233647e05ad7
nao20010128nao <nao20010128@gmail.com>
2016-10-16 10:42:16 +0900

    変更 スタイル強制適用

--- app/src/main/java/com/nao20010128nao/Wisecraft/FragmentSettingsActivity.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/FragmentSettingsActivity.java
@@ -244,6 +244,19 @@ public class FragmentSettingsActivity extends AppCompatActivity {
 		public void onCreatePreferences(Bundle p1, String p2) {
 			// TODO: Implement this method
 			addPreferencesFromResource(R.xml.settings_basic_compat);
+			sH("parallels", new HandledPreference.OnClickListener(){
+					public void onClick(String a, String b, String c) {
+						PreferenceUtils.showEditTextDialog(getActivity(),findPreference("changeDpi"),"1.0",new Treatment<View>(){
+							public void process(View v){
+								EditText text=(EditText)v.findViewById(android.R.id.edit);
+								text.setInputType(InputType.TYPE_CLASS_NUMBER|
+																							InputType.TYPE_TEXT_VARIATION_NORMAL|
+																							InputType.TYPE_NUMBER_FLAG_DECIMAL);
+								text.setFilters(new InputFilter[]{new InputFilter.LengthFilter(3)});
+							}
+						});
+					}
+				});
 			sH("serverListStyle", new HandledPreference.OnClickListener(){
 					public void onClick(String a, String b, String c) {
 						new AppCompatAlertDialog.Builder(getContext(),ThemePatcher.getDefaultDialogStyle(getContext()))
