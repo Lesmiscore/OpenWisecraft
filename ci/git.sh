bce26390846f3650c3241f5fe49d734e2c57574b
nao20010128nao <nao20010128@gmail.com>
2016-10-30 08:17:08 +0900

    変更

--- ci/git.sh
+++ ci/git.sh
@@ -2,21 +2,21 @@ git config --global user.email "nao20010128@gmail.com"
 git config --global user.name "nao20010128nao"
 git submodule update --init --recursive
 cd wisecraft-i18n
-git pull origin master > /dev/null
-git checkout master > /dev/null
+git pull origin master
+git checkout master
 cd ../statusesLayout
-git pull origin master > /dev/null
-git checkout master > /dev/null
+git pull origin master
+git checkout master
 cd ../MaterialIcons
-git pull origin master > /dev/null
-git checkout master > /dev/null
+git pull origin master
+git checkout master
 cd ../calligraphy
-git pull origin master > /dev/null
-git checkout master > /dev/null
+git pull origin master
+git checkout master
 cd ../psts
-git pull origin master > /dev/null
-git checkout master > /dev/null
+git pull origin master
+git checkout master
 cd ../colorPicker
-git pull origin master > /dev/null
-git checkout master > /dev/null
+git pull origin master
+git checkout master
 cd ../
--- ci/prepare.sh
+++ ci/prepare.sh
@@ -2,7 +2,7 @@ apt-get --quiet update --yes > /dev/null
 apt-get --quiet upgrade --yes > /dev/null
 apt-get --quiet install --yes --force-yes wget tar unzip lib32stdc++6 lib32z1 gzip tree > /dev/null
 chmod a+x ./ci/*
-./ci/git.sh 2> /dev/null
+./ci/git.sh 2> /dev/null > /dev/null
 ./ci/sdk.sh > /dev/null
 export POW_OF_2=false
 java -version
