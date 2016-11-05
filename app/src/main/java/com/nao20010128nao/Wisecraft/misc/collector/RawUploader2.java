7fa3c9de0c14959acbb74d912aef27ff64502d5b
nao20010128nao <nao20010128@gmail.com>
2016-11-05 10:52:09 +0000

    データチェック

--- app/src/main/java/com/nao20010128nao/Wisecraft/misc/collector/RawUploader2.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/collector/RawUploader2.java
@@ -77,7 +77,7 @@ public class RawUploader2 implements CollectorMainUploaderProvider {
 				dos.writeInt(baos.size());//Gzipped length
 				dos.write(baos.toByteArray());//Compressed content
 				dos.flush();
-				return true;
+				return sock.getInputStream().read()==0;
 			}catch(Throwable e){
 				WisecraftError.report("RawUploader2",e);
 				return false;
