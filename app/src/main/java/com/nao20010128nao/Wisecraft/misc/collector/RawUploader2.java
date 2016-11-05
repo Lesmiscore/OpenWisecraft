0ef05d124127313826884d6a3b48ffbdd0ce85ad
nao20010128nao <nao20010128@gmail.com>
2016-11-05 10:22:31 +0900

    サーバーにチェックサム検証付きでアップロード サーバー側が未実装なので読み込まないコード

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/collector/RawUploader2.java
@@ -0,0 +1,76 @@
+package com.nao20010128nao.Wisecraft.misc.collector;
+
+import com.nao20010128nao.Wisecraft.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.zip.*;
+import com.nao20010128nao.Wisecraft.misc.compat.*;
+import java.security.*;
+
+public class RawUploader2 implements CollectorMainUploaderProvider {
+
+	@Override
+	public boolean isAvailable() throws Throwable {
+		Socket sock=null;
+		try{
+			sock=new Socket("160.16.112.184",8083);
+			sock.getOutputStream().write(7);
+			return true;
+		}catch(Throwable e){
+			WisecraftError.report("RawUploader2",e);
+			return false;
+		}finally{
+			if(sock!=null)sock.close();
+		}
+	}
+
+	@Override
+	public CollectorMainUploaderProvider.Interface forInterface() {
+		return new Interface();
+	}
+
+
+	class Interface implements CollectorMainUploaderProvider.Interface {
+
+		@Override
+		public void init() throws Throwable {
+			// no-op at present
+		}
+
+		@Override
+		public boolean doUpload(String uuid, String filename, String content) throws Throwable {
+			//we'll upload a file over TCP with GZIP-compressed
+			//server will decompress GZIP and store it
+			byte[] contentBytes=content.getBytes(CompatCharsets.UTF_8);
+			MessageDigest md=MessageDigest.getInstance("sha-256");
+			md.update(uuid.getBytes(CompatCharsets.UTF_8));
+			md.update(filename.getBytes(CompatCharsets.UTF_8));
+			md.update(contentBytes);
+			byte[] hashed=md.digest();
+			Socket sock=null;DataOutputStream dos=null;GZIPOutputStream gos=null;
+			try{
+				sock=new Socket("160.16.112.184",8083);
+				dos=new DataOutputStream(sock.getOutputStream());
+				dos.writeByte(8);
+				dos.writeUTF(uuid);
+				dos.writeUTF(filename);
+				dos.write(hashed);//32bytes
+				dos.writeInt(contentBytes.length);
+				dos.flush();
+				gos=new GZIPOutputStream(dos);
+				gos.flush();
+				gos.write(contentBytes);
+				gos.flush();
+				return true;
+			}catch(Throwable e){
+				WisecraftError.report("RawUploader2",e);
+				return false;
+			}finally{
+				if(gos!=null)gos.close();
+				if(dos!=null)dos.close();
+				if(sock!=null)sock.close();
+			}
+		}		
+	}
+}
