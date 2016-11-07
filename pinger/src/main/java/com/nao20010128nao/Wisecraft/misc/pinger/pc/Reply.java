5100a64bbac36cfb1ad3a13383050446d23419c8
nao20010128nao <nao20010128@gmail.com>
2016-11-07 17:01:53 +0900

    ReplyにSerializedName追加

--- pinger/src/main/java/com/nao20010128nao/Wisecraft/misc/pinger/pc/Reply.java
+++ pinger/src/main/java/com/nao20010128nao/Wisecraft/misc/pinger/pc/Reply.java
@@ -1,5 +1,6 @@
 package com.nao20010128nao.Wisecraft.misc.pinger.pc;
 
+import com.google.gson.annotations.*;
 import com.nao20010128nao.Wisecraft.misc.compat.*;
 import com.nao20010128nao.Wisecraft.misc.pinger.*;
 import java.util.*;
@@ -9,37 +10,53 @@ import java.util.*;
  * https://gist.github.com/thinkofdeath/6927216
  */
 public class Reply implements ServerPingResult,PCQueryResult {
+	@SerializedName("description")
 	public String description;
+	@SerializedName("players")
 	public Players players;
+	@SerializedName("version")
 	public Version version;
+	@SerializedName("favicon")
 	public String favicon;
+	@SerializedName("modinfo")
 	public ModInfo modinfo;
 	
 	private String raw;
 	
 	public class Players {
+		@SerializedName("max")
 		public int max;
+		@SerializedName("online")
 		public int online;
+		@SerializedName("sample")
 		public List<Player> sample;
 	}
 
 	public class Player {
+		@SerializedName("name")
 		public String name;
+		@SerializedName("id")
 		public String id;
 	}
 
 	public class Version {
+		@SerializedName("name")
 		public String name;
+		@SerializedName("protocol")
 		public int protocol;
 	}
 	
 	public class ModInfo{
+		@SerializedName("type")
 		public String type;
+		@SerializedName("modList")
 		public ModListContent[] modList;
 	}
 	
 	public class ModListContent{
+		@SerializedName("modid")
 		public String modid;
+		@SerializedName("version")
 		public String version;
 	}
 
