8f04a4dad511977383b0282a809374d96865c2c5
nao20010128nao <nao20010128@gmail.com>
2016-11-11 17:15:34 +0900

    六重奏

---
+++ compat/src/main/java/com/nao20010128nao/Wisecraft/misc/SextetWalker.java
@@ -0,0 +1,32 @@
+package com.nao20010128nao.Wisecraft.misc;
+import java.util.*;
+
+import static com.nao20010128nao.Wisecraft.misc.CompatUtils.*;
+
+public class SextetWalker<A,B,C,D,E,F> extends ArrayList<Sextet<A,B,C,D,E,F>> 
+{
+	public Sextet<A,B,C,D,E,F> findByA(A o){
+		for(Sextet<A,B,C,D,E,F> i:this)if(CompatUtils.equals(o,i.getA()))return i;
+		return null;
+	}
+	public Sextet<A,B,C,D,E,F> findByB(B o){
+		for(Sextet<A,B,C,D,E,F> i:this)if(CompatUtils.equals(o,i.getB()))return i;
+		return null;
+	}
+	public Sextet<A,B,C,D,E,F> findByC(C o){
+		for(Sextet<A,B,C,D,E,F> i:this)if(CompatUtils.equals(o,i.getC()))return i;
+		return null;
+	}
+	public Sextet<A,B,C,D,E,F> findByD(D o){
+		for(Sextet<A,B,C,D,E,F> i:this)if(CompatUtils.equals(o,i.getD()))return i;
+		return null;
+	}
+	public Sextet<A,B,C,D,E,F> findByE(E o){
+		for(Sextet<A,B,C,D,E,F> i:this)if(CompatUtils.equals(o,i.getE()))return i;
+		return null;
+	}
+	public Sextet<A,B,C,D,E,F> findByF(F o){
+		for(Sextet<A,B,C,D,E,F> i:this)if(CompatUtils.equals(o,i.getF()))return i;
+		return null;
+	}
+}
