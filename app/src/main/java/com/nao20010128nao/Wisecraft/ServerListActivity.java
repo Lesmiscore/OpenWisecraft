47ecda7f734cb2c7bc55eed80e7ae9035360d7a0
nao20010128nao <nao20010128@gmail.com>
2016-11-16 14:20:26 +0900

    いろいろ変更

--- app/src/main/java/com/nao20010128nao/Wisecraft/ServerListActivity.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/ServerListActivity.java
@@ -408,16 +408,17 @@ abstract class ServerListActivityImpl extends ServerListActivityBase1 implements
 									}).
 									show();
 							}
-						},new Treatment<ServerListActivity>(){
-							public void process(ServerListActivity a) {
-								new AlertDialog.Builder(a)
-									 .setItems(0,new DialogInterface.OnClickListener(){
-										 public void onClick(DialogInterface di,int w){
-											 
-										 }
-									 });
-							}
-						},null,UUID.fromString("a077e0fe-1ae0-3e05-8ca1-08b587fb787d")));//0
+						},new DialogLauncherListener<ServerListActivity>(this)
+							.setItems(R.array.serverAddSubMenu,new DialogInterface.OnClickListener(){
+								public void onClick(DialogInterface di,int w){
+									switch(w){
+										case 0:
+											startMultipleDeleteMode();
+											break;
+									}
+								}
+							})
+						,null,UUID.fromString("a077e0fe-1ae0-3e05-8ca1-08b587fb787d")));//0
 		appMenu.add(new Sextet<Integer,Integer,Treatment<ServerListActivity>,Treatment<ServerListActivity>,IDrawerItem,UUID>(R.string.addFromMCPE, R.drawable.ic_add_black_48dp, new Treatment<ServerListActivity>(){
 							public void process(ServerListActivity a) {
 								new AlertDialog.Builder(a,ThemePatcher.getDefaultDialogStyle(a))
--- app/src/main/java/com/nao20010128nao/Wisecraft/misc/DialogLauncherListener.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/DialogLauncherListener.java
@@ -7,12 +7,12 @@ import android.view.*;
 
 import android.support.v7.app.AlertDialog;
 
-public class DialogLauncherListener implements DialogInterface.OnClickListener,View.OnClickListener 
+public class DialogLauncherListener<T> implements DialogInterface.OnClickListener,View.OnClickListener,Treatment<T>
 {
 	AlertDialog.Builder builder;
 	
 	public DialogLauncherListener(Activity a){
-		builder=new AlertDialog.Builder(a);
+		builder=new AlertDialog.Builder(a,ThemePatcher.getDefaultDialogStyle(a));
 	}
 
 	@Override
@@ -24,168 +24,173 @@ public class DialogLauncherListener implements DialogInterface.OnClickListener,V
 	public void onClick(DialogInterface p1, int p2) {
 		builder.show();
 	}
+
+	@Override
+	public void process(T value) {
+		builder.show();
+	}
 	
 	@android.support.annotation.NonNull()
 	public android.content.Context getContext() {
 		return builder.getContext();
 	}
 
-	public DialogLauncherListener setTitle(int titleId) {
+	public DialogLauncherListener<T> setTitle(int titleId) {
 		builder.setTitle(titleId);
 		return this;
 	}
 
-	public DialogLauncherListener setTitle(java.lang.CharSequence title) {
+	public DialogLauncherListener<T> setTitle(java.lang.CharSequence title) {
 		builder.setTitle(title);
 		return this;
 	}
 
-	public DialogLauncherListener setCustomTitle(android.view.View customTitleView) {
+	public DialogLauncherListener<T> setCustomTitle(android.view.View customTitleView) {
 		builder.setCustomTitle(customTitleView);
 		return this;
 	}
 
-	public DialogLauncherListener setMessage(int messageId) {
+	public DialogLauncherListener<T> setMessage(int messageId) {
 		builder.setMessage(messageId);
 		return this;
 	}
 
-	public DialogLauncherListener setMessage(java.lang.CharSequence message) {
+	public DialogLauncherListener<T> setMessage(java.lang.CharSequence message) {
 		builder.setMessage(message);
 		return this;
 	}
 
-	public DialogLauncherListener setIcon(int iconId) {
+	public DialogLauncherListener<T> setIcon(int iconId) {
 		builder.setIcon(iconId);
 		return this;
 	}
 
-	public DialogLauncherListener setIcon(android.graphics.drawable.Drawable icon) {
+	public DialogLauncherListener<T> setIcon(android.graphics.drawable.Drawable icon) {
 		builder.setIcon(icon);
 		return this;
 	}
 
-	public DialogLauncherListener setIconAttribute(int attrId) {
+	public DialogLauncherListener<T> setIconAttribute(int attrId) {
 		builder.setIconAttribute(attrId);
 		return this;
 	}
 
-	public DialogLauncherListener setPositiveButton(int textId, android.content.DialogInterface.OnClickListener listener) {
+	public DialogLauncherListener<T> setPositiveButton(int textId, android.content.DialogInterface.OnClickListener listener) {
 		builder.setPositiveButton(textId,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setPositiveButton(java.lang.CharSequence text, android.content.DialogInterface.OnClickListener listener) {
+	public DialogLauncherListener<T> setPositiveButton(java.lang.CharSequence text, android.content.DialogInterface.OnClickListener listener) {
 		builder.setPositiveButton(text,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setNegativeButton(int textId, android.content.DialogInterface.OnClickListener listener) {
+	public DialogLauncherListener<T> setNegativeButton(int textId, android.content.DialogInterface.OnClickListener listener) {
 		builder.setNegativeButton(textId,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setNegativeButton(java.lang.CharSequence text, android.content.DialogInterface.OnClickListener listener) {
+	public DialogLauncherListener<T> setNegativeButton(java.lang.CharSequence text, android.content.DialogInterface.OnClickListener listener) {
 		builder.setNegativeButton(text,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setNeutralButton(int textId, android.content.DialogInterface.OnClickListener listener) {
+	public DialogLauncherListener<T> setNeutralButton(int textId, android.content.DialogInterface.OnClickListener listener) {
 		builder.setNeutralButton(textId,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setNeutralButton(java.lang.CharSequence text, android.content.DialogInterface.OnClickListener listener) {
+	public DialogLauncherListener<T> setNeutralButton(java.lang.CharSequence text, android.content.DialogInterface.OnClickListener listener) {
 		builder.setNeutralButton(text,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setCancelable(boolean cancelable) {
+	public DialogLauncherListener<T> setCancelable(boolean cancelable) {
 		builder.setCancelable(cancelable);
 		return this;
 	}
 
-	public DialogLauncherListener setOnCancelListener(android.content.DialogInterface.OnCancelListener onCancelListener) {
+	public DialogLauncherListener<T> setOnCancelListener(android.content.DialogInterface.OnCancelListener onCancelListener) {
 		builder.setOnCancelListener(onCancelListener);
 		return this;
 	}
 
-	public DialogLauncherListener setOnDismissListener(android.content.DialogInterface.OnDismissListener onDismissListener) {
+	public DialogLauncherListener<T> setOnDismissListener(android.content.DialogInterface.OnDismissListener onDismissListener) {
 		builder.setOnDismissListener(onDismissListener);
 		return this;
 	}
 
-	public DialogLauncherListener setOnKeyListener(android.content.DialogInterface.OnKeyListener onKeyListener) {
+	public DialogLauncherListener<T> setOnKeyListener(android.content.DialogInterface.OnKeyListener onKeyListener) {
 		builder.setOnKeyListener(onKeyListener);
 		return this;
 	}
 
-	public DialogLauncherListener setItems(int itemsId, android.content.DialogInterface.OnClickListener listener) {
+	public DialogLauncherListener<T> setItems(int itemsId, android.content.DialogInterface.OnClickListener listener) {
 		builder.setItems(itemsId,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setItems(java.lang.CharSequence[] items, android.content.DialogInterface.OnClickListener listener) {
+	public DialogLauncherListener<T> setItems(java.lang.CharSequence[] items, android.content.DialogInterface.OnClickListener listener) {
 		builder.setItems(items,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setAdapter(android.widget.ListAdapter adapter, android.content.DialogInterface.OnClickListener listener) {
+	public DialogLauncherListener<T> setAdapter(android.widget.ListAdapter adapter, android.content.DialogInterface.OnClickListener listener) {
 		builder.setAdapter(adapter,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setCursor(android.database.Cursor cursor, android.content.DialogInterface.OnClickListener listener, java.lang.String labelColumn) {
+	public DialogLauncherListener<T> setCursor(android.database.Cursor cursor, android.content.DialogInterface.OnClickListener listener, java.lang.String labelColumn) {
 		builder.setCursor(cursor,listener,labelColumn);
 		return this;
 	}
 
-	public DialogLauncherListener setMultiChoiceItems(int itemsId, boolean[] checkedItems, android.content.DialogInterface.OnMultiChoiceClickListener listener) {
+	public DialogLauncherListener<T> setMultiChoiceItems(int itemsId, boolean[] checkedItems, android.content.DialogInterface.OnMultiChoiceClickListener listener) {
 		builder.setMultiChoiceItems(itemsId,checkedItems,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setMultiChoiceItems(java.lang.CharSequence[] items, boolean[] checkedItems, android.content.DialogInterface.OnMultiChoiceClickListener listener) {
+	public DialogLauncherListener<T> setMultiChoiceItems(java.lang.CharSequence[] items, boolean[] checkedItems, android.content.DialogInterface.OnMultiChoiceClickListener listener) {
 		builder.setMultiChoiceItems(items,checkedItems,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setMultiChoiceItems(android.database.Cursor cursor, java.lang.String isCheckedColumn, java.lang.String labelColumn, android.content.DialogInterface.OnMultiChoiceClickListener listener) {
+	public DialogLauncherListener<T> setMultiChoiceItems(android.database.Cursor cursor, java.lang.String isCheckedColumn, java.lang.String labelColumn, android.content.DialogInterface.OnMultiChoiceClickListener listener) {
 		builder.setMultiChoiceItems(cursor,isCheckedColumn,labelColumn,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setSingleChoiceItems(int itemsId, int checkedItem, android.content.DialogInterface.OnClickListener listener) {
+	public DialogLauncherListener<T> setSingleChoiceItems(int itemsId, int checkedItem, android.content.DialogInterface.OnClickListener listener) {
 		builder.setSingleChoiceItems(itemsId,checkedItem,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setSingleChoiceItems(android.database.Cursor cursor, int checkedItem, java.lang.String labelColumn, android.content.DialogInterface.OnClickListener listener) {
+	public DialogLauncherListener<T> setSingleChoiceItems(android.database.Cursor cursor, int checkedItem, java.lang.String labelColumn, android.content.DialogInterface.OnClickListener listener) {
 		builder.setSingleChoiceItems(cursor,checkedItem,labelColumn,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setSingleChoiceItems(java.lang.CharSequence[] items, int checkedItem, android.content.DialogInterface.OnClickListener listener) {
+	public DialogLauncherListener<T> setSingleChoiceItems(java.lang.CharSequence[] items, int checkedItem, android.content.DialogInterface.OnClickListener listener) {
 		builder.setSingleChoiceItems(items,checkedItem,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setSingleChoiceItems(android.widget.ListAdapter adapter, int checkedItem, android.content.DialogInterface.OnClickListener listener) {
+	public DialogLauncherListener<T> setSingleChoiceItems(android.widget.ListAdapter adapter, int checkedItem, android.content.DialogInterface.OnClickListener listener) {
 		builder.setSingleChoiceItems(adapter,checkedItem,listener);
 		return this;
 	}
 
-	public DialogLauncherListener setOnItemSelectedListener(android.widget.AdapterView.OnItemSelectedListener listener) {
+	public DialogLauncherListener<T> setOnItemSelectedListener(android.widget.AdapterView.OnItemSelectedListener listener) {
 		builder.setOnItemSelectedListener(listener);
 		return this;
 	}
 
-	public DialogLauncherListener setView(int layoutResId) {
+	public DialogLauncherListener<T> setView(int layoutResId) {
 		builder.setView(layoutResId);
 		return this;
 	}
 
-	public DialogLauncherListener setView(android.view.View view) {
+	public DialogLauncherListener<T> setView(android.view.View view) {
 		builder.setView(view);
 		return this;
 	}
@@ -193,20 +198,20 @@ public class DialogLauncherListener implements DialogInterface.OnClickListener,V
 	/** @deprecated */
 	@java.lang.Deprecated()
 	@android.support.annotation.RestrictTo(value={android.support.annotation.RestrictTo.Scope.GROUP_ID})
-	public DialogLauncherListener setView(android.view.View view, int viewSpacingLeft, int viewSpacingTop, int viewSpacingRight, int viewSpacingBottom) {
+	public DialogLauncherListener<T> setView(android.view.View view, int viewSpacingLeft, int viewSpacingTop, int viewSpacingRight, int viewSpacingBottom) {
 		builder.setView(view,viewSpacingLeft,viewSpacingTop,viewSpacingRight,viewSpacingBottom);
 		return this;
 	}
 
 	/** @deprecated */
 	@java.lang.Deprecated()
-	public DialogLauncherListener setInverseBackgroundForced(boolean useInverseBackground) {
+	public DialogLauncherListener<T> setInverseBackgroundForced(boolean useInverseBackground) {
 		builder.setInverseBackgroundForced(useInverseBackground);
 		return this;
 	}
 
 	@android.support.annotation.RestrictTo(value={android.support.annotation.RestrictTo.Scope.GROUP_ID})
-	public DialogLauncherListener setRecycleOnMeasureEnabled(boolean enabled) {
+	public DialogLauncherListener<T> setRecycleOnMeasureEnabled(boolean enabled) {
 		builder.setRecycleOnMeasureEnabled(enabled);
 		return this;
 	}
