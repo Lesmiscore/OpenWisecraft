1d809a300a2229c21eadd2dd31b9b05121fe68de
nao20010128nao <nao20010128@gmail.com>
2016-11-16 14:49:16 +0900

    main以外は何もしない

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -1,8 +1,10 @@
-apt-get --quiet update --yes > /dev/null
-apt-get --quiet upgrade --yes > /dev/null
-apt-get --quiet install --yes --force-yes wget tar unzip lib32stdc++6 lib32z1 gzip tree > /dev/null
-chmod a+x ./ci/*
-./ci/git.sh 2> /dev/null > /dev/null
-source ./ci/sdk.sh > /dev/null
-export POW_OF_2=false
-java -version
+if [${CI_BUILD_STAGE} = "main"]; then
+	apt-get --quiet update --yes > /dev/null
+	apt-get --quiet upgrade --yes > /dev/null
+	apt-get --quiet install --yes --force-yes wget tar unzip lib32stdc++6 lib32z1 gzip tree > /dev/null
+	chmod a+x ./ci/*
+	./ci/git.sh 2> /dev/null > /dev/null
+	source ./ci/sdk.sh > /dev/null
+	export POW_OF_2=false
+	java -version
+fi
