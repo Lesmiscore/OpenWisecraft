d131c99530025d94d1d3b280f3d5a4b95111445f
nao20010128nao <nao20010128@gmail.com>
2016-11-20 10:05:50 +0900

    変更

--- rcon/src/main/java/com/nao20010128nao/Wisecraft/rcon/KeyChain.java
+++ rcon/src/main/java/com/nao20010128nao/Wisecraft/rcon/KeyChain.java
@@ -2,6 +2,6 @@ package com.nao20010128nao.Wisecraft.rcon;
 
 public interface KeyChain
 {
-	public boolean isPasswordExists(String ip,int port);
+	public boolean isPasswordStored(String ip,int port);
 	public String[] getRecentPassword(String ip,int port);
 }
