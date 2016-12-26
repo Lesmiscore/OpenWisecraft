b80cd84c631b8f196100e2383c752167954f5584
nao20010128nao <nao20010128@gmail.com>
2016-12-26 01:58:17 +0000

    追加

--- ci/sdk.sh
+++ ci/sdk.sh
@@ -1,6 +1,6 @@
 wget --quiet --output-document=android-sdk.tgz https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
 tar --extract --gzip --file=android-sdk.tgz
-echo y | android-sdk-linux/tools/android --silent update sdk --no-ui --all --filter platform-tools,tools,build-tools-25.0.0,android-25,extra-android-m2repository,extra-google-m2repository
+echo y | android-sdk-linux/tools/android --silent update sdk --no-ui --all --filter platform-tools,tools,build-tools-25.0.0,android-25,android-24,extra-android-m2repository,extra-google-m2repository
 wget --quiet --output-document=gradle.zip https://services.gradle.org/distributions/gradle-2.14.1-bin.zip
 unzip -q gradle.zip
 export ANDROID_HOME=$PWD/android-sdk-linux
