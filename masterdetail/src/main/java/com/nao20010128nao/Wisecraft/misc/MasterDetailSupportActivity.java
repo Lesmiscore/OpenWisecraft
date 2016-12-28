ad177d8eabc1de58e3246756bb8a2bebeb732eff
nao20010128nao <nao20010128@gmail.com>
2016-12-28 17:15:52 +0900

    MasterDetailのテンプレ

---
+++ masterdetail/src/main/java/com/nao20010128nao/Wisecraft/misc/MasterDetailSupportActivity.java
@@ -0,0 +1,37 @@
+package com.nao20010128nao.Wisecraft.misc;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public abstract class MasterDetailSupportActivity extends AppCompatActivity {
+
+    private boolean mTwoPane=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        View recyclerView = findViewById(R.id.item_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+
+        if (findViewById(R.id.item_detail_container) != null) {
+            mTwoPane = true;
+        }
+    }
+
+    public boolean isTwoPane(){
+        return mTwoPane;
+    }
+
+    public abstract void setupRecyclerView(@NonNull RecyclerView recyclerView);
+}
