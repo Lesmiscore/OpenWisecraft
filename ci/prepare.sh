4e82fa28654b2427b4dbe56e786a8e916e0655fb
nao20010128nao <nao20010128@gmail.com>
2017-01-08 22:47:28 +0900

    変更

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$CI_BUILD_STAGE" == "main" ]
 then
-    apt-get install tree
+    apt-get install tree zip unzip > /dev/null
 	./ci/git.sh 2> /dev/null > /dev/null
 	export POW_OF_2=false
 	java -version
