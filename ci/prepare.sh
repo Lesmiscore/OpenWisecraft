3f42b1ac4bb3aeba1466351e15d5a0bb87b72938
nao20010128nao <nao20010128@gmail.com>
2017-01-12 23:16:51 +0900

    修正 Bash on Ubuntu on Windowsで再現したら動くんだけどなぁ

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo $CI_BUILD_STAGE
 if [[ $CI_BUILD_STAGE == main* ]]
 then
+	echo "Preparing for build..."
     apt-get install --yes tree zip unzip p7zip p7zip-full > /dev/null
 	./ci/git.sh 2> /dev/null > /dev/null
 	export POW_OF_2=false
