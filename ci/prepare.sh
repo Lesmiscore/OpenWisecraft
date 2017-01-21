a3c496929c868e15764058015a83fc211c3aaa97
nao20010128nao <nao20010128@gmail.com>
2017-01-22 02:08:33 +0900

    Split APKの生成 ビルドをコピーするだけでできるように名前で判定

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 echo $CI_BUILD_STAGE
 echo $CI_BUILD_NAME
-if [[ $CI_BUILD_STAGE != postBuild ]]
+if [[ ${CI_BUILD_STAGE:-} != postBuild ]]
 then
 	echo "Preparing for build..."
     apt-get install --yes tree zip unzip p7zip p7zip-full > /dev/null
 	./ci/git.sh
 	export POW_OF_2=false
+	if [[ ${CI_BUILD_NAME:-} == *Split ]]
+	then
+		export SPLIT_APK=true
+	else
+		export SPLIT_APK=false
+	fi
 	java -version
 fi
