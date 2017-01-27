7c696ecdde0567f2b89bff40beda91dc073486b2
nao20010128nao <nao20010128@gmail.com>
2017-01-27 16:17:38 +0900

    面倒くさいから削除

--- app/src/main/java/com/nao20010128nao/Wisecraft/misc/DialogLauncherListener.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/DialogLauncherListener.java
@@ -216,12 +216,6 @@ public class DialogLauncherListener<T> implements DialogInterface.OnClickListene
 		builder.setInverseBackgroundForced(useInverseBackground);
 		return this;
 	}
-
-	@android.support.annotation.RestrictTo(value={android.support.annotation.RestrictTo.Scope.GROUP_ID})
-	public DialogLauncherListener<T> setRecycleOnMeasureEnabled(boolean enabled) {
-		builder.setRecycleOnMeasureEnabled(enabled);
-		return this;
-	}
 	
 	
 	
