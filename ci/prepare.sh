c1065931b7d19c35f727a4b69551cb14376f9ac8
nao20010128nao <nao20010128@gmail.com>
2017-01-29 22:10:46 +0900

    Jackビルド用の変数自動認識に対応 なおJackビルドはできない模様

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -4,10 +4,9 @@ echo $CI_BUILD_NAME
 if [[ ${CI_BUILD_STAGE:-} != postBuild ]]
 then
 	echo "Preparing for build..."
-    apt-get install --yes tree zip unzip p7zip p7zip-full tor > /dev/null
+    apt-get install --yes tree zip unzip p7zip p7zip-full > /dev/null
 	./ci/git.sh
-	export POW_OF_2=false
-	if [[ ${CI_BUILD_NAME:-} == *Split ]]
+	if [[ ${CI_BUILD_NAME:-} == *Split* ]]
 	then
 		export SPLIT_APK=true
 		echo "Split APK build is enabled"
@@ -15,6 +14,15 @@ then
 		export SPLIT_APK=false
 		echo "Split APK build is disabled"
 	fi
+	if [[ ${CI_BUILD_NAME:-} == *Jack* ]]
+	then
+		export JACK_BUILD=true
+		echo "Jack build is enabled"
+	else
+		export JACK_BUILD=false
+		echo "Jack build is disabled"
+	fi
+	export POW_OF_2=false
 	export USE_TOR=false
 	java -version
 fi
