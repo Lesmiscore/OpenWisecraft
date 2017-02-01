622ebd63b0ccdcbfd17a62ec51a6022598f7dabc
nao20010128nao <nao20010128@gmail.com>
2017-02-01 23:56:15 +0900

    追加

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/BottomSheetListDialog.java
@@ -0,0 +1,58 @@
+package com.nao20010128nao.Wisecraft.misc;
+
+import android.content.*;
+import android.support.annotation.*;
+import android.support.v7.widget.*;
+import android.widget.*;
+import com.nao20010128nao.Wisecraft.*;
+
+public class BottomSheetListDialog extends ModifiedBottomSheetDialog {
+	private RecyclerView rv;
+	private TextView title;
+	
+	public BottomSheetListDialog(@NonNull Context context) {
+        super(context);
+		init(context);
+    }
+
+    public BottomSheetListDialog(@NonNull Context context, @StyleRes int theme) {
+        super(context, theme);
+        init(context);
+    }
+	
+	private void init(Context context){
+		setContentView(R.layout.bottom_sheet_list_dialog);
+		rv=(RecyclerView)findViewById(R.id.list);
+		title=(TextView)findViewById(R.id.title);
+	}
+
+	@Override
+	public void setTitle(CharSequence title0) {
+		title.setText(title0);
+	}
+
+	@Override
+	public void setTitle(int titleId) {
+		title.setText(titleId);
+	}
+	
+	public RecyclerView getRecyclerView(){
+		return rv;
+	}
+	
+	public RecyclerView.Adapter getAdapter(){
+		return rv.getAdapter();
+	}
+	
+	public void setAdapter(RecyclerView.Adapter adap){
+		rv.setAdapter(adap);
+	}
+
+	public RecyclerView.LayoutManager getLayoutManager(){
+		return rv.getLayoutManager();
+	}
+
+	public void setLayoutManager(RecyclerView.LayoutManager lm){
+		rv.setLayoutManager(lm);
+	}
+}
