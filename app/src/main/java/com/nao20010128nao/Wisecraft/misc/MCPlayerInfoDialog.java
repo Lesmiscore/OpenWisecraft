a031d7f38bee78f9287bfe0c4e5453b8bb12c527
nao20010128nao <nao20010128@gmail.com>
2017-02-11 16:12:19 +0900

    プレイヤー情報

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/MCPlayerInfoDialog.java
@@ -0,0 +1,107 @@
+package com.nao20010128nao.Wisecraft.misc;
+
+import android.graphics.*;
+import android.os.*;
+import android.support.v7.app.*;
+import android.widget.*;
+import com.nao20010128nao.Wisecraft.*;
+import com.nao20010128nao.Wisecraft.misc.skin_face.*;
+import java.io.*;
+import java.lang.ref.*;
+import java.net.*;
+import java.util.regex.*;
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+import java.util.concurrent.atomic.*;
+
+abstract class MCPlayerInfoDialogImpl extends AppCompatDialog {
+	String player;
+	TextView username,uuid,reputation;
+	ImageView face;
+	Handler h=new Handler();
+	
+	public MCPlayerInfoDialogImpl(android.content.Context context) {
+		this(context,ThemePatcher.getDefaultDialogStyle(context));
+	}
+
+    public MCPlayerInfoDialogImpl(android.content.Context context, int theme) {
+		super(context,theme);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.minecraft_pc_user_info);
+		face=(ImageView)findViewById(R.id.face);
+		username=(TextView)findViewById(R.id.username);
+		uuid=(TextView)findViewById(R.id.uuid);
+		reputation=(TextView)findViewById(R.id.mcbansReputation);
+		username.setText(player);
+		new Thread(){
+			String uuidText=null;
+			Bitmap bmp=null;
+			String rept=null;
+			public void run(){
+				try {
+					uuidText=UUIDFetcher.getUUIDOf(player).toString();
+				} catch (Exception e) {
+					WisecraftError.report("MCPlayerInfoDialog",e);
+				}
+				try{
+					try(InputStream is=new URL("https://crafatar.com/avatars/"+(uuidText!=null?uuidText:player)).openConnection().getInputStream()){
+						bmp=BitmapFactory.decodeStream(is);
+					}
+					Bitmap beforeEdit=bmp;
+					bmp=ImageResizer.resizeBitmapPixel(bmp,face.getLayoutParams().width/8);
+					beforeEdit.recycle();
+				}catch(Throwable e){
+					WisecraftError.report("MCPlayerInfoDialog",e);
+				}
+				try{
+					if(uuidText!=null){
+						Document doc=Jsoup.connect("http://mcbans.com/player/"+uuidText+"/")
+							.userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36")
+							.get();
+						Elements elems=doc.select("fieldset > section > div.section-content");
+						for(Element element:elems){
+							String value=element.text();
+							if(value.matches("^(0|1|2|3|4|5|6|7|8|9|10)(|\\.[0-9]+)"+Pattern.quote(" / 10")+"$")){
+								rept=value;
+								break;
+							}
+						}
+					}
+				}catch(Throwable e){
+					WisecraftError.report("MCPlayerInfoDialog",e);
+				}
+				h.post(new Runnable(){
+						public void run(){
+							uuid.setText(uuidText);
+							reputation.setText(rept);
+							face.setImageBitmap(bmp);
+						}
+					});
+			}
+		}.start();
+	}
+	
+	public void setPlayer(String player) {
+		this.player = player;
+	}
+
+	public String getPlayer() {
+		return player;
+	}
+	
+}
+public class MCPlayerInfoDialog extends MCPlayerInfoDialogImpl{
+	public MCPlayerInfoDialog(android.content.Context context) {
+		super(context);
+	}
+
+    public MCPlayerInfoDialog(android.content.Context context, int theme) {
+		super(context,theme);
+	}
+}
+
