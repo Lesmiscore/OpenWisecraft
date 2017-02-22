2a070aeffe70477e13b49f9e12aa6c21fabc0d3c
nao20010128nao <nao20010128@gmail.com>
2017-02-22 11:22:21 +0900

    追加

---
+++ app/src/main/java/com/nao20010128nao/Wisecraft/services/ServerFinderService.java
@@ -0,0 +1,20 @@
+package com.nao20010128nao.Wisecraft.services;
+import android.app.*;
+import android.os.*;
+import android.content.*;
+
+public class ServerFinderService extends Service
+{
+
+	@Override
+	public IBinder onBind(Intent p1) {
+		// TODO: Implement this method
+		return null;
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO: Implement this method
+		return super.onStartCommand(intent, flags, startId);
+	}
+}
