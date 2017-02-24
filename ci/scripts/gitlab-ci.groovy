e6ffd45b1ad63d5631a02e050d12417ae447927b
nao20010128nao <nao20010128@gmail.com>
2017-02-24 16:07:42 +0900

    変更

--- ci/scripts/gitlab-ci.groovy
+++ ci/scripts/gitlab-ci.groovy
@@ -1,67 +1,67 @@
-image="nao20010128nao/android-build:latest"
-before_script=["chmod a+x ./ci/*", "source ./ci/prepare.sh"]
-stages=["app","rcon","postBuild"]
+image="nao20010128nao/android-build:latest".toString()
+before_script=["chmod a+x ./ci/*".toString(), "source ./ci/prepare.sh".toString()]
+stages=["app".toString(),"rcon".toString(),"postBuild".toString()]
 def artf=[
-      "*.apk",
-      "dexdump*.txt",
-      "build-*.log",
-      "powof2.txt",
-      "*/build/outputs/aar/*",
-      "tree-*.log",
-      "*/build/outputs/mapping/*/*.txt",
-      "*/build/generated/source/*",
-      "autogen-*.*"
+      "*.apk".toString(),
+      "dexdump*.txt".toString(),
+      "build-*.log".toString(),
+      "powof2.txt".toString(),
+      "*/build/outputs/aar/*".toString(),
+      "tree-*.log".toString(),
+      "*/build/outputs/mapping/*/*.txt".toString(),
+      "*/build/generated/source/*".toString(),
+      "autogen-*.*".toString()
 ]
 def mainScript=[
-      "gradle safeDeleteDevXml \$TASK --info --stacktrace | tee -a build-\$CI_BUILD_NAME.log > /dev/null",
-      "mv */build/outputs/apk/*.apk .",
-      "./ci/packageInformations.sh"
+      "gradle safeDeleteDevXml \$TASK --info --stacktrace | tee -a build-\$CI_BUILD_NAME.log > /dev/null".toString(),
+      "mv */build/outputs/apk/*.apk .".toString(),
+      "./ci/packageInformations.sh".toString()
 ]
 def preScript=[
-      "DIR=`cat ./app/build.gradle | grep \"def preNum=\"`",
-      "DIR=\${DIR% //*}",
-      "DIR=\${DIR#*=}",
-      "if [ \$DIR != \"0\" ]; then gradle safeDeleteDevXml \$TASK --info --stacktrace | tee -a build-\$CI_BUILD_NAME.log > /dev/null; mv */build/outputs/apk/*.apk . ;./ci/packageInformations.sh ; fi"
+      "DIR=`cat ./app/build.gradle | grep \"def preNum=\"`".toString(),
+      "DIR=\${DIR% //*}".toString(),
+      "DIR=\${DIR#*=}".toString(),
+      "if [ \$DIR != \"0\" ]; then gradle safeDeleteDevXml \$TASK --info --stacktrace | tee -a build-\$CI_BUILD_NAME.log > /dev/null; mv */build/outputs/apk/*.apk . ;./ci/packageInformations.sh ; fi".toString()
 ]
-["","Split"].each{split->
+["".toString(),"Split".toString()].each{split->
     // app
-    def app=["Debug","Debug2","Debug2TestObs","Pg","PgUlt","PgExpermental","Release","ShrinkRelease"]
+    def app=["Debug".toString(),"Debug2".toString(),"Debug2TestObs".toString(),"Pg".toString(),"PgUlt".toString(),"PgExpermental".toString(),"Release".toString(),"ShrinkRelease".toString()]
     app.each{
-        this["app$it$split"].script=mainScript
-        this["app$it$split"].artifacts.paths=artf
-        this["app$it$split"].stage="app"
-        this["app$it$split"].variables.TASK=":app:assemble$it"
+        this["app$it$split".toString()].script=mainScript
+        this["app$it$split".toString()].artifacts.paths=artf
+        this["app$it$split".toString()].stage="app".toString()
+        this["app$it$split".toString()].variables.TASK=":app:assemble$it".toString()
     }
-    def pre=["","Shrink"]
+    def pre=["".toString(),"Shrink".toString()]
     pre.each{
-        this["app$it$split"].script=preScript
-        this["app$it$split"].artifacts.paths=artf
-        this["app$it$split"].stage="app"
-        this["app$it$split"].variables.TASK=":app:assemble${it}Pre"
+        this["app${it}Pre$split".toString()].script=preScript
+        this["app${it}Pre$split".toString()].artifacts.paths=artf
+        this["app${it}Pre$split".toString()].stage="app".toString()
+        this["app${it}Pre$split".toString()].variables.TASK=":app:assemble${it}Pre".toString()
     }
     // rcon
-    def rcon=["App","PassCrack"]
+    def rcon=["App".toString(),"PassCrack".toString()]
     rcon.each{
-        this["rcon${it}All$split"].script=mainScript
-        this["rcon${it}All$split"].artifacts.paths=artf
-        this["rcon${it}All$split"].stage="rcon"
-        this["rcon${it}All$split"].variables.TASK=":app:assemble$it"
+        this["rcon${it}All$split".toString()].script=mainScript
+        this["rcon${it}All$split".toString()].artifacts.paths=artf
+        this["rcon${it}All$split".toString()].stage="rcon".toString()
+        this["rcon${it}All$split".toString()].variables.TASK=":app:assemble$it".toString()
     }
 }
 // postBuild
 mainLast.artifacts.paths=artf
 mainLast.stage="postBuild"
 mainLast.script=[
-      "unzip app-release.apk -d unzippedApkA > /dev/null",
-      "\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkA/classes.dex > dexdump-app.txt",
-      "unzip rconApp-release.apk -d unzippedApkB > /dev/null",
-      "\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkB/classes.dex > dexdump-rcon.txt",
-      "unzip rconPassCrack-release.apk -d unzippedApkC > /dev/null",
-      "\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkC/classes.dex > dexdump-rconPC.txt",
-      "unzip app-pg.apk -d unzippedApkD > /dev/null",
-      "\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkD/classes.dex > dexdump-appObs.txt",
-      "grep \"Git\\sRevision\" build-appDebug.log"
+      "unzip app-release.apk -d unzippedApkA > /dev/null".toString(),
+      "\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkA/classes.dex > dexdump-app.txt".toString(),
+      "unzip rconApp-release.apk -d unzippedApkB > /dev/null".toString(),
+      "\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkB/classes.dex > dexdump-rcon.txt".toString(),
+      "unzip rconPassCrack-release.apk -d unzippedApkC > /dev/null".toString(),
+      "\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkC/classes.dex > dexdump-rconPC.txt".toString(),
+      "unzip app-pg.apk -d unzippedApkD > /dev/null".toString(),
+      "\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkD/classes.dex > dexdump-appObs.txt".toString(),
+      "grep \"Git\\sRevision\" build-appDebug.log".toString()
 ]
 
-copy.stage="postBuild"
-copy.script=["source ./ci/copy.sh"]
+copy.stage="postBuild".toString()
+copy.script=["source ./ci/copy.sh".toString()]
