ebc0e3fa2a13f3e0aa5aa603ceb854e35a47a58b
nao20010128nao <nao20010128@gmail.com>
2017-02-28 16:40:48 +0900

    追加

--- app/src/main/java/com/nao20010128nao/Wisecraft/misc/MCSkinViewerDialog.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/MCSkinViewerDialog.java
@@ -1,10 +1,15 @@
 package com.nao20010128nao.Wisecraft.misc;
 
 import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
 import android.os.*;
 import android.support.v7.app.*;
+import android.support.v7.view.*;
+import com.nao20010128nao.Wisecraft.*;
 import fi.iki.elonen.*;
 import fi.iki.elonen.NanoHTTPD.*;
+import java.net.*;
 import java.util.regex.*;
 
 public class MCSkinViewerDialog extends AppCompatDialog 
@@ -22,6 +27,7 @@ public class MCSkinViewerDialog extends AppCompatDialog
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.skin_viewer_dialog);
 	}
 
 	public void setPlayer(String player) {
@@ -30,6 +36,17 @@ public class MCSkinViewerDialog extends AppCompatDialog
 
 	public String getPlayer() {
 		return player;
+	}
+	
+	private String getBackgroundColor(){
+		TypedArray ta=new ContextThemeWrapper(getContext(),ThemePatcher.getDefaultDialogStyle(getContext())).obtainStyledAttributes(new int[]{android.R.id.background});
+		int color;
+		try{
+			color= ta.getColor(0,Color.BLACK);
+		}finally{
+			ta.recycle();
+		}
+		return String.format("#%06X", 0xFFFFFF & color);
 	}
 	
 	
@@ -41,6 +58,21 @@ public class MCSkinViewerDialog extends AppCompatDialog
 
 		@Override
 		public NanoHTTPD.Response serve(NanoHTTPD.IHTTPSession session) {
+			if(session.getUri().endsWith("/skins/image.png")){
+				try{
+					return newChunkedResponse(cast(Response.Status.OK),Utils.getMimeType(".png"),new URL("http://crafater.com/skins/"+player).openStream());
+				}catch(Throwable e){
+					
+				}
+			}
+			if(session.getUri().endsWith("/background.css")){
+				StringBuilder css=new StringBuilder();
+				css
+				.append("baccground{")
+				.append("background-color:").append(getBackgroundColor()).append(";")
+				.append("}");
+				return newFixedLengthResponse(css.toString());
+			}
 			try{
 				String path;
 				if(session.getUri().startsWith("/")){
