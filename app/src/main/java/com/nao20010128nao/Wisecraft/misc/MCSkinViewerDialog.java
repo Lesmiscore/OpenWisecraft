4b739466961037741448ab3ee5fde3a005c956e5
nao20010128nao <nao20010128@gmail.com>
2017-02-28 20:37:07 +0900

    WebGL対応確認関連

--- app/src/main/java/com/nao20010128nao/Wisecraft/misc/MCSkinViewerDialog.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/misc/MCSkinViewerDialog.java
@@ -6,15 +6,22 @@ import android.graphics.*;
 import android.os.*;
 import android.support.v7.app.*;
 import android.support.v7.view.*;
+import android.view.*;
+import android.webkit.*;
+import android.widget.*;
 import com.nao20010128nao.Wisecraft.*;
 import fi.iki.elonen.*;
 import fi.iki.elonen.NanoHTTPD.*;
+import java.io.*;
 import java.net.*;
 import java.util.regex.*;
 
+import android.support.v7.view.ContextThemeWrapper;
 public class MCSkinViewerDialog extends AppCompatDialog 
 {
 	String player;
+	FrameLayout progress;
+	WebView skinViewer;
 	public MCSkinViewerDialog(android.content.Context context) {
 		this(context,ThemePatcher.getDefaultDialogStyle(context));
 	}
@@ -28,6 +35,17 @@ public class MCSkinViewerDialog extends AppCompatDialog
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.skin_viewer_dialog);
+		progress=(FrameLayout)findViewById(R.id.loading);
+		skinViewer=(WebView)findViewById(R.id.skinViewingWebView);
+		
+		try {
+			new SkinViewerHttpServer().start(5000, true);
+			skinViewer.setWebViewClient(new WebViewClient(){});
+			skinViewer.getSettings().setJavaScriptEnabled(true);
+			skinViewer.loadUrl("http://localhost:6432/");
+		} catch (IOException e) {
+			WisecraftError.report("MCSkinViewerDialog",e);
+		}
 	}
 
 	public void setPlayer(String player) {
@@ -52,12 +70,35 @@ public class MCSkinViewerDialog extends AppCompatDialog
 	
 	class SkinViewerHttpServer extends NanoHTTPD{
 		Context c=getContext();
+		boolean webGlChecked=false;
 		public SkinViewerHttpServer(){
 			super(65432);
 		}
 
 		@Override
 		public NanoHTTPD.Response serve(NanoHTTPD.IHTTPSession session) {
+			if(session.getUri().endsWith("/xhr/webgl_available")){
+				// Web GL is available so show WebView
+				getOwnerActivity().runOnUiThread(new Runnable(){
+						public void run(){
+							progress.setVisibility(View.GONE);
+							skinViewer.setVisibility(View.VISIBLE);
+						}
+					});
+				webGlChecked=true;
+				return newFixedLengthResponse("");
+			}
+			if(session.getUri().endsWith("/xhr/webgl_bad")){
+				// Web GL is NOT available so we use another browser
+				getOwnerActivity().runOnUiThread(new Runnable(){
+						public void run(){
+							progress.setVisibility(View.GONE);
+							skinViewer.setVisibility(View.VISIBLE);
+						}
+					});
+				webGlChecked=true;
+				return newFixedLengthResponse("");
+			}
 			if(session.getUri().endsWith("/skins/image.png")){
 				try{
 					return newChunkedResponse(cast(Response.Status.OK),Utils.getMimeType(".png"),new URL("http://crafater.com/skins/"+player).openStream());
