abd6f8928f24c71254e29b408e85c81f8cb4e8ca
nao20010128nao <nao20010128@gmail.com>
2017-03-15 20:19:13 +0900

    ドメインチェックと表示

--- app/src/main/java/com/nao20010128nao/Wisecraft/activity/ServerListActivityBase7.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/activity/ServerListActivityBase7.java
@@ -2,6 +2,7 @@ package com.nao20010128nao.Wisecraft.activity;
 
 import android.graphics.drawable.*;
 import android.os.*;
+import android.support.v4.view.*;
 import android.support.v4.widget.*;
 import android.support.v7.view.*;
 import android.support.v7.widget.*;
@@ -10,10 +11,11 @@ import android.widget.*;
 import com.google.common.collect.*;
 import com.nao20010128nao.Wisecraft.*;
 import com.nao20010128nao.Wisecraft.misc.*;
+import java.net.*;
 import java.util.*;
 
 import android.support.v7.view.ActionMode;
-import android.support.v4.view.*;
+
 //Remove servers on unused domains
 public abstract class ServerListActivityBase7 extends ServerListActivityBaseFields 
 {
@@ -25,6 +27,9 @@ public abstract class ServerListActivityBase7 extends ServerListActivityBaseFiel
 			RecyclerView.Adapter slaDefaultAdapter;
 			RecyclerView.LayoutManager slaDefaultLayoutManager;
 			Drawable slaViewBackground;
+			Multimap<String,Server> domains;
+			DomainListAdapter domainLstAdptr;
+			DomainStatusChecker worker;
 			
             public boolean onCreateActionMode(ActionMode p1, Menu p2) {
                 srl.setEnabled(false);
@@ -33,9 +38,12 @@ public abstract class ServerListActivityBase7 extends ServerListActivityBaseFiel
 				slaDefaultAdapter=rv.getAdapter();
 				slaDefaultLayoutManager=rv.getLayoutManager();
 				Multimap<String,Server> domains=listDomains();
-				rv.setAdapter(new DomainListAdapter(domains));
+				domainLstAdptr=new DomainListAdapter(domains);
+				rv.setAdapter(domainLstAdptr);
 				rv.setLayoutManager(new LinearLayoutManager(ServerListActivityBase7.this));
 				ViewCompat.setBackground(rv,ThemePatcher.getWindowBackground(ServerListActivityBase7.this));
+				(worker=new DomainStatusChecker()).execute(domainLstAdptr);
+				this.domains=domains;
 				isInSelectMode=false;
 				editMode = EDIT_MODE_REMOVE_UNUSED_DOMAINS;
                 return true;
@@ -58,6 +66,9 @@ public abstract class ServerListActivityBase7 extends ServerListActivityBaseFiel
 				rv.setAdapter(slaDefaultAdapter);
 				rv.setLayoutManager(slaDefaultLayoutManager);
 				ViewCompat.setBackground(rv,slaViewBackground);
+				if(worker.getStatus()==AsyncTask.Status.RUNNING){
+					worker.cancel(true);
+				}
                 saveServers();
 				isInSelectMode=false;
             }
@@ -86,17 +97,25 @@ public abstract class ServerListActivityBase7 extends ServerListActivityBaseFiel
 		Multimap<String,Server> domains;
 		List<String> listedDomains;
 		List<Boolean> expandStates;
+		List<Boolean> domainChecked;
+		List<Boolean> domainUsed;
+		List<Boolean> deletingDomain;
 		
 		public DomainListAdapter(Multimap<String,Server> domains){
 			this.domains=domains;
-			listedDomains=new ArrayList<>(domains.keySet());
-			expandStates=new ArrayList<>(Collections.nCopies(listedDomains.size(),false));
+			List<Boolean> nCopied=Collections.nCopies(listedDomains.size(),false);
+			listedDomains=  Collections.unmodifiableList(new ArrayList<>(domains.keySet()));
+			expandStates=   Collections.unmodifiableList(new ArrayList<>(nCopied));
+			domainChecked=  Collections.unmodifiableList(new ArrayList<>(nCopied));
+			domainUsed=     Collections.unmodifiableList(new ArrayList<>(nCopied));
+			deletingDomain= Collections.unmodifiableList(new ArrayList<>(nCopied));
 		}
 		
 		@Override
 		public void onBindViewHolder(Vh p1, final int p2) {
 			p1.domain.setText(listedDomains.get(p2));
 			p1.servers.removeAllViews();
+			p1.available.setImageDrawable(TheApplication.instance.getTintedDrawable(R.drawable.ic_check_black_48dp,ThemePatcher.getMainColor(ServerListActivityBase7.this)));
 			if(expandStates.get(p2)){
 				p1.servers.setVisibility(View.VISIBLE);
 				for(Server serv:domains.get(listedDomains.get(p2))){
@@ -109,6 +128,18 @@ public abstract class ServerListActivityBase7 extends ServerListActivityBaseFiel
 			}else{
 				p1.servers.setVisibility(View.GONE);
 				p1.expand.setImageDrawable(TheApplication.instance.getTintedDrawable(R.drawable.ic_expand_more_black_48dp,p1.domain.getTextColors().getDefaultColor()));
+			}
+			if(domainChecked.get(p2)){
+				if(domainUsed.get(p2)){
+					//check mark
+					p1.showAvailable();
+				}else{
+					//checkbox to ask user to delete
+					p1.showWillDelete(deletingDomain.get(p2));
+				}
+			}else{
+				//loading
+				p1.showLoading();
 			}
 			p1.expand.setOnClickListener(new View.OnClickListener(){
 					public void onClick(View v){
@@ -133,12 +164,71 @@ public abstract class ServerListActivityBase7 extends ServerListActivityBaseFiel
 		final ImageButton expand;
 		final TextView domain;
 		final LinearLayout servers;
+		final FrameLayout domainStatus;
+		final ProgressBar loading;
+		final CheckBox willDelete;
+		final ImageView available;
 
 		public Vh(View v){
 			super(v);
 			expand=findTypedViewById(R.id.expand);
 			domain=findTypedViewById(R.id.domain);
 			servers=findTypedViewById(R.id.servers);
+			domainStatus=findTypedViewById(R.id.domainStatus);
+			loading=findTypedViewById(R.id.loading);
+			willDelete=findTypedViewById(R.id.willDelete);
+			available=findTypedViewById(R.id.available);
+		}
+		
+		public void showLoading(){
+			loading.setVisibility(View.VISIBLE);
+			willDelete.setVisibility(View.GONE);
+			available.setVisibility(View.GONE);
+		}
+		
+		public void showWillDelete(boolean value){
+			loading.setVisibility(View.GONE);
+			willDelete.setVisibility(View.VISIBLE);
+			available.setVisibility(View.GONE);
+			willDelete.setChecked(value);
+		}
+		
+		public void showAvailable(){
+			loading.setVisibility(View.GONE);
+			willDelete.setVisibility(View.GONE);
+			available.setVisibility(View.VISIBLE);
+		}
+	}
+	
+	class DomainStatusChecker extends AsyncTask<DomainListAdapter,Map.Entry<Integer,Boolean>,Void> {
+		DomainListAdapter adapter;
+		
+		@Override
+		protected Void doInBackground(DomainListAdapter[] p1) {
+			adapter=p1[0];
+			List<String> domains=adapter.listedDomains;
+			for(String dm:domains){
+				if(isCancelled())break;
+				boolean ok=false;
+				try {
+					InetAddress.getAllByName(dm);
+					ok=true;
+				} catch (UnknownHostException e) {}
+				publishProgress(new KVP<>(domains.indexOf(dm),ok));
+			}
+			return null;
+		}
+
+		@Override
+		protected void onProgressUpdate(Map.Entry<Integer, Boolean>[] values) {
+			for(Map.Entry<Integer, Boolean> updates:values){
+				int offset=updates.getKey();
+				boolean value=updates.getValue();
+				adapter.domainChecked.set(offset,true);
+				adapter.domainUsed.set(offset,value);
+				adapter.deletingDomain.set(offset,false);
+				adapter.notifyItemChanged(offset);
+			}
 		}
 	}
 }
