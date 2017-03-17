c311a58ba4ea56e68df29947dce717cbfebb5fa0
nao20010128nao <nao20010128@gmail.com>
2017-03-17 22:13:52 +0900

    修正

--- app/src/main/java/com/nao20010128nao/Wisecraft/activity/ServerListActivityBase7.java
+++ app/src/main/java/com/nao20010128nao/Wisecraft/activity/ServerListActivityBase7.java
@@ -51,7 +51,7 @@ public abstract class ServerListActivityBase7 extends ServerListActivityBaseFiel
             public boolean onPrepareActionMode(ActionMode p1, Menu p2) {
                 editMode = EDIT_MODE_REMOVE_UNUSED_DOMAINS;
 				MenuItem delete=p2.add(0,0,0,R.string.delete)
-					.setIcon(TheApplication.instance.getTintedDrawable(R.drawable.ic_delete_forever_black_48dp,ThemePatcher.getMainColor(ServerListActivityBase7.this)));
+					.setIcon(TheApplication.instance.getTintedDrawable(R.drawable.ic_delete_forever_black_48dp,ThemePatcher.getMenuTintColor(ServerListActivityBase7.this)));
 				MenuItemCompat.setShowAsAction(delete,MenuItem.SHOW_AS_ACTION_ALWAYS);
                 return true;
             }
