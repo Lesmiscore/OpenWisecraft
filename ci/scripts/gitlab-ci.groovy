61136c981344376ae8154819db4a9435a1737444
nao20010128nao <nao20010128@gmail.com>
2017-04-21 12:45:44 +0900

    色々変更

--- ci/scripts/gitlab-ci.groovy
+++ ci/scripts/gitlab-ci.groovy
@@ -1,26 +1,26 @@
-image="nao20010128nao/android-build:latest".toString()
-before_script=["chmod a+x ./ci/*".toString(), "source ./ci/prepare.sh".toString()]
-stages=["app".toString(),"rcon".toString(),"postBuild".toString()]
+image='nao20010128nao/android-build:latest'
+before_script=['chmod a+x ./ci/*', 'source ./ci/prepare.sh']
+stages=['app','rcon','postBuild']
 def artf=[
-      "*.apk".toString(),
-      "build-*.log".toString(),
-      "powof2.txt".toString(),
-      "*/build/outputs/aar/*".toString(),
-      "tree-*.log".toString(),
-      "*/build/outputs/mapping/*/*.txt".toString(),
-      "*/build/generated/source/*".toString(),
-      "autogen-*.*".toString()
+      '*.apk',
+      'build-*.log',
+      'powof2.txt',
+      '*/build/outputs/aar/*',
+      'tree-*.log',
+      '*/build/outputs/mapping/*/*.txt',
+      '*/build/generated/source/*',
+      'autogen-*.*'
 ]
 def mainScript=[
-      "gradle safeDeleteDevXml \$TASK --info --stacktrace | tee -a build-\$CI_BUILD_NAME.log > /dev/null".toString(),
-      "mv */build/outputs/apk/*.apk .".toString(),
-      "./ci/packageInformations.sh".toString()
+      'gradle safeDeleteDevXml \$TASK --info --stacktrace | tee -a build-\$CI_BUILD_NAME.log > /dev/null',
+      'mv */build/outputs/apk/*.apk .',
+      './ci/packageInformations.sh'
 ]
 def preScript=[
-      "DIR=`cat ./app/build.gradle | grep \"def preNum=\"`".toString(),
-      "DIR=\${DIR% //*}".toString(),
-      "DIR=\${DIR#*=}".toString(),
-      "if [ \$DIR != \"0\" ]; then gradle safeDeleteDevXml \$TASK --info --stacktrace | tee -a build-\$CI_BUILD_NAME.log > /dev/null; mv */build/outputs/apk/*.apk . ;./ci/packageInformations.sh ; fi".toString()
+      'DIR=`cat ./app/build.gradle | grep \"def preNum=\"`',
+      'DIR=\${DIR% //*}',
+      'DIR=\${DIR#*=}',
+      'if [ \$DIR != \"0\" ]; then gradle safeDeleteDevXml \$TASK --info --stacktrace | tee -a build-\$CI_BUILD_NAME.log > /dev/null; mv */build/outputs/apk/*.apk . ;./ci/packageInformations.sh ; fi'
 ]
 ["","Shksknvwr"].each{ssv->
 	["","Split"].each{split->
@@ -29,47 +29,53 @@ def preScript=[
         app.each{
             this["app$it$split$ssv".toString()].script=mainScript
             this["app$it$split$ssv".toString()].artifacts.paths=artf
-            this["app$it$split$ssv".toString()].stage="app".toString()
+            this["app$it$split$ssv".toString()].artifacts.when='always'
+            this["app$it$split$ssv".toString()].stage='app'
             this["app$it$split$ssv".toString()].variables.TASK=":app:assemble$it".toString()
         }
         def pre=["","Shrink"]
         pre.each{
             this["app${it}Pre$split$ssv".toString()].script=preScript
             this["app${it}Pre$split$ssv".toString()].artifacts.paths=artf
-            this["app${it}Pre$split$ssv".toString()].stage="app".toString()
+            this["app${it}Pre$split$ssv".toString()].artifacts.when='always'
+            this["app${it}Pre$split$ssv".toString()].stage='app'
             this["app${it}Pre$split$ssv".toString()].variables.TASK=":app:assemble${it}Pre".toString()
         }
         // rcon
         def rcon=["App":["Release","Pre"],"PassCrack":["Release"]]
-        rcon.entrySet().each{kv->
+        rcon.each{kv->
             def module=kv.key
      	   kv.value.each{build->
      	       this["rcon$module$build$split$ssv".toString()].script=mainScript
                 this["rcon$module$build$split$ssv".toString()].artifacts.paths=artf
-                this["rcon$module$build$split$ssv".toString()].stage="rcon".toString()
+                this["rcon$module$build$split$ssv".toString()].artifacts.when='always'
+                this["rcon$module$build$split$ssv".toString()].stage='rcon'
                 this["rcon$module$build$split$ssv".toString()].variables.TASK=":rcon${module}:assemble$build".toString()
             }
         }
     }
 }
 // postBuild
-mainLast.artifacts.paths=["dexdump*.txt".toString()]
-mainLast.stage="postBuild"
+mainLast.artifacts.paths=['dexdump*.txt']
+mainLast.stage='postBuild'
 mainLast.script=[
-      "unzip app-release.apk -d unzippedApkA > /dev/null".toString(),
-      "\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkA/classes.dex > dexdump-app.txt".toString(),
-      "unzip rconApp-release.apk -d unzippedApkB > /dev/null".toString(),
-      "\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkB/classes.dex > dexdump-rcon.txt".toString(),
-      "unzip rconPassCrack-release.apk -d unzippedApkC > /dev/null".toString(),
-      "\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkC/classes.dex > dexdump-rconPC.txt".toString(),
-      "unzip app-pg.apk -d unzippedApkD > /dev/null".toString(),
-      "\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkD/classes.dex > dexdump-appObs.txt".toString(),
-      "grep \"Git\\sRevision\" build-appDebug.log".toString()
+      'unzip app-release.apk -d unzippedApkA > /dev/null',
+      '\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkA/classes.dex > dexdump-app.txt',
+      'unzip rconApp-release.apk -d unzippedApkB > /dev/null".toString(),
+      '\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkB/classes.dex > dexdump-rcon.txt',
+      'unzip rconPassCrack-release.apk -d unzippedApkC > /dev/null',
+      '\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkC/classes.dex > dexdump-rconPC.txt',
+      'unzip app-pg.apk -d unzippedApkD > /dev/null',
+      '\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkD/classes.dex > dexdump-appObs.txt',
+      'grep \"Git\\sRevision\" build-appDebug.log'
 ]
 
-copy.stage="postBuild".toString()
-copy.script=["source ./ci/copy.sh".toString()]
+copy.stage='postBuild'
+copy.script=['source ./ci/copy.sh']
 
-convertTest.stage="postBuild".toString()
-convertTest.script=["./ci/git.sh".toString(),"gradle convertConfigSlurperToBuildscript".toString()]
-convertTest.artifacts.paths=["test.gitlab-ci.yml".toString()]
+convertTest.stage='postBuild'
+convertTest.script=['./ci/git.sh','gradle convertConfigSlurperToBuildscript']
+convertTest.artifacts.paths=['test.gitlab-ci.yml']
+convertTest.artifacts.when='always'
+convertTest.when='always'
+convertTest.dependencies=[]
