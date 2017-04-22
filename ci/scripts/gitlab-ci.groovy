d7ec3d879c30db63444388ac73fb0560ae3bfbf6
nao20010128nao <nao20010128@gmail.com>
2017-04-22 10:45:23 +0000

    Revert "変更"
    This reverts commit 83f6211f6fdad9b3d1bad8ce5a69fa8bceb7f0d1

--- ci/scripts/gitlab-ci.groovy
+++ ci/scripts/gitlab-ci.groovy
@@ -27,81 +27,55 @@ def preScript=[
         // app
         def app=["Debug","Debug2","Debug2TestObs","Pg","PgUlt","PgExperimental","Release","ShrinkRelease"]
         app.each{
-            this["app$it$split$ssv".toString()]{
-                script=mainScript
-                artifacts{
-                    paths=artf
-                    when='always'
-                }
-                stage='app'
-                variables.TASK=":app:assemble$it".toString()
-                dependencies=[]
-            }
+            this["app$it$split$ssv".toString()].script=mainScript
+            this["app$it$split$ssv".toString()].artifacts.paths=artf
+            this["app$it$split$ssv".toString()].artifacts.when='always'
+            this["app$it$split$ssv".toString()].stage='app'
+            this["app$it$split$ssv".toString()].variables.TASK=":app:assemble$it".toString()
         }
         def pre=["","Shrink"]
         pre.each{
-            this["app${it}Pre$split$ssv".toString()]{
-                script=preScript
-                artifacts{
-                    paths=artf
-                    when='always'
-                }
-                stage='app'
-                variables.TASK=":app:assemble${it}Pre".toString()
-                dependencies=[]
-            }
+            this["app${it}Pre$split$ssv".toString()].script=preScript
+            this["app${it}Pre$split$ssv".toString()].artifacts.paths=artf
+            this["app${it}Pre$split$ssv".toString()].artifacts.when='always'
+            this["app${it}Pre$split$ssv".toString()].stage='app'
+            this["app${it}Pre$split$ssv".toString()].variables.TASK=":app:assemble${it}Pre".toString()
         }
         // rcon
         def rcon=["App":["Release","Pre"],"PassCrack":["Release"]]
         rcon.each{kv->
             def module=kv.key
      	   kv.value.each{build->
-     	       this["rcon$module$build$split$ssv".toString()]{
-                    script=mainScript
-                    artifacts{
-                        paths=artf
-                        when='always'
-                    }
-                    stage='rcon'
-                    variables.TASK=":rcon${module}:assemble$build".toString()
-                    dependencies=[]
-                }
+     	       this["rcon$module$build$split$ssv".toString()].script=mainScript
+                this["rcon$module$build$split$ssv".toString()].artifacts.paths=artf
+                this["rcon$module$build$split$ssv".toString()].artifacts.when='always'
+                this["rcon$module$build$split$ssv".toString()].stage='rcon'
+                this["rcon$module$build$split$ssv".toString()].variables.TASK=":rcon${module}:assemble$build".toString()
             }
         }
     }
 }
 // postBuild
-mainLast{
-    artifacts{
-        paths=['dexdump*.txt']
-    }
-    stage='postBuild'
-    script=[
-        'unzip app-release.apk -d unzippedApkA > /dev/null',
-        '\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkA/classes.dex > dexdump-app.txt',
-        'unzip rconApp-release.apk -d unzippedApkB > /dev/null',
-        '\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkB/classes.dex > dexdump-rcon.txt',
-        'unzip rconPassCrack-release.apk -d unzippedApkC > /dev/null',
-        '\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkC/classes.dex > dexdump-rconPC.txt',
-        'unzip app-pg.apk -d unzippedApkD > /dev/null',
-        '\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkD/classes.dex > dexdump-appObs.txt',
-        'grep \"Git\\sRevision\" build-appDebug.log'
-    ]
-    allow_failure=true
-}
-copy{
-    stage='postBuild'
-    script=['source ./ci/copy.sh']
-    dependencies=[]
-}
+mainLast.artifacts.paths=['dexdump*.txt']
+mainLast.stage='postBuild'
+mainLast.script=[
+      'unzip app-release.apk -d unzippedApkA > /dev/null',
+      '\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkA/classes.dex > dexdump-app.txt',
+      'unzip rconApp-release.apk -d unzippedApkB > /dev/null',
+      '\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkB/classes.dex > dexdump-rcon.txt',
+      'unzip rconPassCrack-release.apk -d unzippedApkC > /dev/null',
+      '\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkC/classes.dex > dexdump-rconPC.txt',
+      'unzip app-pg.apk -d unzippedApkD > /dev/null',
+      '\$ANDROID_HOME/build-tools/25.0.0/dexdump -d unzippedApkD/classes.dex > dexdump-appObs.txt',
+      'grep \"Git\\sRevision\" build-appDebug.log'
+]
 
-convertTest{
-    stage='postBuild'
-    script=['./ci/git.sh','gradle convertConfigSlurperToBuildscript']
-    artifacts{
-        paths=['test.gitlab-ci.yml']
-        when='always'
-    }
-    dependencies=[]
-    when='always'
-}
+copy.stage='postBuild'
+copy.script=['source ./ci/copy.sh']
+
+convertTest.stage='postBuild'
+convertTest.script=['./ci/git.sh','gradle convertConfigSlurperToBuildscript']
+convertTest.artifacts.paths=['test.gitlab-ci.yml']
+convertTest.artifacts.when='always'
+convertTest.when='always'
+convertTest.dependencies=[]
