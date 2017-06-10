91f7733ff8d8debe3ab3cf88b4c107af56c004aa
nao20010128nao <nao20010128@gmail.com>
2017-06-10 15:52:38 +0900

    修正

--- ci/clone.sh
+++ ci/clone.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 while : ; do
-  [ ! -e Wisecraft/.git ] && rm -rf Wisecraft
+  [ ! -e Wisecraft/.git ] || ERROR && rm -rf Wisecraft
   [ ! -e Wisecraft ] && git clone $WISECRAFT_ENDPOINT > build.log 2>&1
   cd Wisecraft
   [ -e ../build.log ] && cp ../build.log .
-  git pull >> build.log 2>&1 || continue
-  git checkout $BRANCH >> build.log 2>&1 || continue
+  git pull >> build.log 2>&1 || { ERROR=1; continue; }
+  git checkout $BRANCH >> build.log 2>&1 || { ERROR=1; continue; }
   chmod a+x ci/*
   ./ci/git.sh >> build.log 2>&1
   break
