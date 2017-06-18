6b2f43218de4243a3e10b3b867304c4bec224378
nao20010128nao <nao20010128@gmail.com>
2017-06-18 18:58:34 +0900

    表示方式を変更 Markdownはビルド時点でrenderしておいてWisecraftは表示だけをする

--- compat/src/main/java/com/nao20010128nao/Wisecraft/misc/OpenSourceActivity2Base.java
+++ compat/src/main/java/com/nao20010128nao/Wisecraft/misc/OpenSourceActivity2Base.java
@@ -3,6 +3,7 @@ package com.nao20010128nao.Wisecraft.misc;
 import android.os.*;
 import android.support.v7.app.*;
 import android.view.*;
+import android.webkit.*;
 import com.nao20010128nao.Wisecraft.misc.compat.BuildConfig;
 import com.nao20010128nao.Wisecraft.misc.compat.R;
 import eu.fiskur.markdownview.*;
@@ -11,17 +12,14 @@ import java.io.*;
 
 public abstract class OpenSourceActivity2Base extends AppCompatActivity
 {
-	MarkdownView markdownView;
-	File oslMdCache;
+	WebView markdownView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.only_toolbar);
 		getLayoutInflater().inflate(R.layout.open_source_markdown,(ViewGroup)findViewById(R.id.frame));
 		setSupportActionBar(CompatUtils.getToolbar(this));
-		markdownView=(MarkdownView)findViewById(R.id.markdownView);
-		oslMdCache=new File(getCacheDir(),"openSourceLicense.md");
-		markdownView.allowGestures(true);
-		markdownView.showMarkdown(BuildConfig.OPEN_SOURCE_LICENSE);
+		markdownView=(WebView)findViewById(R.id.markdownView);
+		markdownView.loadData(BuildConfig.OPEN_SOURCE_LICENSE,"text/html","utf-8");
 	}
 }
